3ae56abacea319b4f3d45fe0561e6f002027d53b
Yogita Ganage <yogitaganage19@gmail.com>
2026-06-01 18:29:48 +0100

    vffd

--- test.sql
+++ test.sql
@@ -1,13 +1,2 @@
-SELECT
-  sc.date_event_recorded,
-  to_timestamp(sc.date_event_recorded, 'dd MMM yyyy HH:mm:ss') AS form_ans_time_id
-FROM silver_sone_srcode sc
-LIMIT 20;
-
-to_timestamp(sc.date_event_recorded, 'dd MMM yyyy HH:mm:ss') AS form_ans_time_id,
-
-
-
-
--- form_ans_time_id currently stores timestamp value from SRCode DateEventRecorded; name may be revised later
-to_timestamp(sc.date_event_recorded, 'dd MMM yyyy HH:mm:ss') AS form_ans_time_id,
+SELECT * from emp1
+WHERE empno = 7369;
