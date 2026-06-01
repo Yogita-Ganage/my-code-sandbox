a7c1eeb9e65bf4535e14a173659530ec3157128a
Yogita Ganage <yogitaganage19@gmail.com>
2026-06-01 18:00:08 +0100

    trsr

--- test.sql
+++ test.sql
@@ -1,14 +1,13 @@
 SELECT
-  ar.created_at,
-  to_timestamp(ar.created_at, "yyyy-MM-dd'T'HH:mm:ss.SSS'Z'") AS form_ans_time_id
-FROM silver_drj_assessment_results ar
+  sc.date_event_recorded,
+  to_timestamp(sc.date_event_recorded, 'dd MMM yyyy HH:mm:ss') AS form_ans_time_id
+FROM silver_sone_srcode sc
 LIMIT 20;
 
-
-
-to_timestamp(ar.created_at, "yyyy-MM-dd'T'HH:mm:ss.SSS'Z'") AS form_ans_time_id,
+to_timestamp(sc.date_event_recorded, 'dd MMM yyyy HH:mm:ss') AS form_ans_time_id,
 
 
 
 
-Added form_ans_time_id for MPB using silver_drj_assessment_results.created_at converted to timestamp format, as confirmed despite the column name containing "id".
+-- form_ans_time_id currently stores timestamp value from SRCode DateEventRecorded; name may be revised later
+to_timestamp(sc.date_event_recorded, 'dd MMM yyyy HH:mm:ss') AS form_ans_time_id,
