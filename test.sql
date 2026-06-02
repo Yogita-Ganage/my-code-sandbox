0846dd2ece11ccdc69e65473cc81eb93ccd12c79
Yogita Ganage <yogitaganage19@gmail.com>
2026-06-02 17:06:57 +0100

    kjiop

--- test.sql
+++ test.sql
@@ -2,3 +2,6 @@ LEFT JOIN temp_wip_form_answer_parsed_date_lookup pdl
     ON pdl.activity_header_id = s.activity_header_id
    AND pdl.group_id = s.group_id
    AND TRIM(LOWER(pdl.group_desc)) = TRIM(LOWER(s.group_desc))
+
+
+,pdl.parsed_form_ans_date AS form_ans_date
