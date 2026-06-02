ba1cb83062a499c734c599ff9fc4063a8a20a742
Yogita Ganage <yogitaganage19@gmail.com>
2026-06-02 17:24:07 +0100

    valon

--- test.sql
+++ test.sql
@@ -5,3 +5,50 @@ LEFT JOIN temp_wip_form_answer_parsed_date_lookup pdl
 
 
 ,pdl.parsed_form_ans_date AS form_ans_date
+
+
+
+
+%%sql
+
+SELECT
+    COUNT(*) AS total_rows,
+    COUNT(form_ans_date) AS populated_form_ans_date_rows,
+    COUNT(*) - COUNT(form_ans_date) AS null_form_ans_date_rows
+FROM wiptest_silver_form_answer
+WHERE z_src_system_id = 'WIP';
+
+
+%%sql
+
+SELECT
+    form_ans_form_ques_id,
+    form_ans_care_epi_id,
+    form_ans_src_answer,
+    form_ans_date,
+    z_src_system_instance,
+    z_src_system_id
+FROM wiptest_silver_form_answer
+WHERE z_src_system_id = 'WIP'
+  AND form_ans_date IS NOT NULL
+LIMIT 50;
+
+
+%%sql
+
+SELECT
+    form_ans_form_ques_id,
+    form_ans_care_epi_id,
+    form_ans_src_answer,
+    form_ans_date,
+    COUNT(*) AS row_count
+FROM wiptest_silver_form_answer
+WHERE z_src_system_id = 'WIP'
+GROUP BY
+    form_ans_form_ques_id,
+    form_ans_care_epi_id,
+    form_ans_src_answer,
+    form_ans_date
+HAVING COUNT(*) > 1
+ORDER BY row_count DESC
+LIMIT 50;
