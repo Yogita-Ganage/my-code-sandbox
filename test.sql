3724013cc9956ecdf5b27ba9cba9d9cdbfef7f5e
Yogita Ganage <yogitaganage19@gmail.com>
2026-06-03 13:07:26 +0100

    gtion

--- test.sql
+++ test.sql
@@ -1,54 +1,30 @@
 %%sql
 
-SELECT
-    activity_header_id,
-    group_id,
-    group_desc,
-    type_id,
-    type_desc,
-    value_desc
-FROM test_temp_silver_wip_activityheader_statistics
-WHERE TRIM(LOWER(type_desc)) IN ('call date', 'session date')
-  AND value_desc IS NOT NULL
-  AND (
-        value_desc RLIKE '21[0-9]{2}'
-     OR value_desc RLIKE '22[0-9]{2}'
-     OR value_desc RLIKE '23[0-9]{2}'
-     OR value_desc RLIKE '24[0-9]{2}'
-     OR value_desc RLIKE '25[0-9]{2}'
-     OR value_desc RLIKE '29[0-9]{2}'
-     OR value_desc RLIKE '30[0-9]{2}'
-  )
-ORDER BY value_desc
-LIMIT 200;
+DROP TABLE IF EXISTS temp_wip_form_answer_parsed_date_lookup;
 
+CREATE TABLE temp_wip_form_answer_parsed_date_lookup AS
+WITH cleaned AS (
+    SELECT
+        activity_header_id,
+        group_id,
+        group_desc,
+        raw_session_date,
 
+        TRIM(
+            REGEXP_REPLACE(
+                REGEXP_REPLACE(
+                    REGEXP_REPLACE(
+                        REGEXP_REPLACE(INITCAP(TRIM(raw_session_date)), '[–—]', '-'),
+                        '^(Monday|Tuesday|Wednesday|Thursday|Friday|Saturday|Sunday)\\s+', ''
+                    ),
+                    '([0-9]{1,2})(st|nd|rd|th)\\b', '$1'
+                ),
+                ',', ''
+            )
+        ) AS clean_session_date
 
-%%sql
-
-SELECT
-    activity_header_id,
-    group_id,
-    group_desc,
-    raw_session_date
-FROM test_temp_wip_form_answer_date_lookup
-WHERE raw_session_date IS NOT NULL
-  AND (
-        raw_session_date RLIKE '21[0-9]{2}'
-     OR raw_session_date RLIKE '22[0-9]{2}'
-     OR raw_session_date RLIKE '23[0-9]{2}'
-     OR raw_session_date RLIKE '24[0-9]{2}'
-     OR raw_session_date RLIKE '25[0-9]{2}'
-     OR raw_session_date RLIKE '29[0-9]{2}'
-     OR raw_session_date RLIKE '30[0-9]{2}'
-  )
-ORDER BY raw_session_date
-LIMIT 200;
-
-
-
-
-%%sql
+    FROM temp_wip_form_answer_date_lookup
+)
 
 SELECT
     activity_header_id,
@@ -56,9 +32,52 @@ SELECT
     group_desc,
     raw_session_date,
     clean_session_date,
-    parsed_form_ans_date
-FROM test_temp_wip_form_answer_parsed_date_lookup
-WHERE parsed_form_ans_date > ADD_MONTHS(CURRENT_DATE(), 120)
-   OR parsed_form_ans_date < DATE '1900-01-01'
-ORDER BY parsed_form_ans_date, raw_session_date
-LIMIT 200;
+
+    CASE
+        WHEN clean_session_date RLIKE '^[0-9]{1,2}/[0-9]{1,2}/[0-9]{4}.*$'
+            THEN TO_DATE(FROM_UNIXTIME(UNIX_TIMESTAMP(REGEXP_EXTRACT(clean_session_date, '^([0-9]{1,2}/[0-9]{1,2}/[0-9]{4})', 1), 'd/M/yyyy')))
+
+        WHEN clean_session_date RLIKE '^[0-9]{1,2}/[0-9]{1,2}/[0-9]{2}.*$'
+            THEN TO_DATE(FROM_UNIXTIME(UNIX_TIMESTAMP(REGEXP_EXTRACT(clean_session_date, '^([0-9]{1,2}/[0-9]{1,2}/[0-9]{2})', 1), 'd/M/yy')))
+
+        WHEN clean_session_date RLIKE '^[0-9]{1,2}\\.[0-9]{1,2}\\.[0-9]{4}.*$'
+            THEN TO_DATE(FROM_UNIXTIME(UNIX_TIMESTAMP(REGEXP_EXTRACT(clean_session_date, '^([0-9]{1,2}\\.[0-9]{1,2}\\.[0-9]{4})', 1), 'd.M.yyyy')))
+
+        WHEN clean_session_date RLIKE '^[0-9]{1,2}\\.[0-9]{1,2}\\.[0-9]{2}.*$'
+            THEN TO_DATE(FROM_UNIXTIME(UNIX_TIMESTAMP(REGEXP_EXTRACT(clean_session_date, '^([0-9]{1,2}\\.[0-9]{1,2}\\.[0-9]{2})', 1), 'd.M.yy')))
+
+        WHEN clean_session_date RLIKE '^[0-9]{1,2}-[0-9]{1,2}-[0-9]{4}.*$'
+            THEN TO_DATE(FROM_UNIXTIME(UNIX_TIMESTAMP(REGEXP_EXTRACT(clean_session_date, '^([0-9]{1,2}-[0-9]{1,2}-[0-9]{4})', 1), 'd-M-yyyy')))
+
+        WHEN clean_session_date RLIKE '^[0-9]{1,2}-[0-9]{1,2}-[0-9]{2}.*$'
+            THEN TO_DATE(FROM_UNIXTIME(UNIX_TIMESTAMP(REGEXP_EXTRACT(clean_session_date, '^([0-9]{1,2}-[0-9]{1,2}-[0-9]{2})', 1), 'd-M-yy')))
+
+        WHEN clean_session_date RLIKE '^[0-9]{1,2} [A-Za-z]{3} [0-9]{4}.*$'
+            THEN TO_DATE(FROM_UNIXTIME(UNIX_TIMESTAMP(REGEXP_EXTRACT(clean_session_date, '^([0-9]{1,2} [A-Za-z]{3} [0-9]{4})', 1), 'd MMM yyyy')))
+
+        WHEN clean_session_date RLIKE '^[0-9]{1,2} [A-Za-z]{3} [0-9]{2}.*$'
+            THEN TO_DATE(FROM_UNIXTIME(UNIX_TIMESTAMP(REGEXP_EXTRACT(clean_session_date, '^([0-9]{1,2} [A-Za-z]{3} [0-9]{2})', 1), 'd MMM yy')))
+
+        WHEN clean_session_date RLIKE '^[0-9]{1,2} [A-Za-z]+ [0-9]{4}.*$'
+            THEN TO_DATE(FROM_UNIXTIME(UNIX_TIMESTAMP(REGEXP_EXTRACT(clean_session_date, '^([0-9]{1,2} [A-Za-z]+ [0-9]{4})', 1), 'd MMMM yyyy')))
+
+        WHEN clean_session_date RLIKE '^[0-9]{1,2} [A-Za-z]+ [0-9]{2}.*$'
+            THEN TO_DATE(FROM_UNIXTIME(UNIX_TIMESTAMP(REGEXP_EXTRACT(clean_session_date, '^([0-9]{1,2} [A-Za-z]+ [0-9]{2})', 1), 'd MMMM yy')))
+
+        WHEN clean_session_date RLIKE '^[A-Za-z]{3} [0-9]{1,2} [0-9]{4}.*$'
+            THEN TO_DATE(FROM_UNIXTIME(UNIX_TIMESTAMP(REGEXP_EXTRACT(clean_session_date, '^([A-Za-z]{3} [0-9]{1,2} [0-9]{4})', 1), 'MMM d yyyy')))
+
+        WHEN clean_session_date RLIKE '^[A-Za-z]+ [0-9]{1,2} [0-9]{4}.*$'
+            THEN TO_DATE(FROM_UNIXTIME(UNIX_TIMESTAMP(REGEXP_EXTRACT(clean_session_date, '^([A-Za-z]+ [0-9]{1,2} [0-9]{4})', 1), 'MMMM d yyyy')))
+
+        ELSE NULL
+    END AS parsed_form_ans_date
+
+FROM cleaned;
+
+
+
+
+
+spark.conf.set("spark.sql.parquet.datetimeRebaseModeInWrite", "CORRECTED")
+spark.conf.set("spark.sql.parquet.int96RebaseModeInWrite", "CORRECTED")
