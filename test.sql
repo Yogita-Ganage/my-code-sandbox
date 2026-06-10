0e5870bad6f078b3f0444c805a69b9b9d2eb41a0
Yogita Ganage <yogitaganage19@gmail.com>
2026-06-10 16:47:53 +0100

    aaa20

--- test.sql
+++ test.sql
@@ -1,1 +1,5 @@
-**Note:** Before running the WIP block, run the Spark configuration cell above first. This is required to allow old/future source date and timestamp values to write successfully.
+Updated WIP `session_patient_id` mapping to align with the attribute definition.
+
+The value is now built from `z_src_system_instance` + `client_id` from `silver_wip_activityheaderroledetail`, instead of using the person table ID.
+
+Validated against example care episode `573340`: `client_id = 304007`, expected `session_patient_id = WIP001304007`.
