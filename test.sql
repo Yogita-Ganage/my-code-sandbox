f14fb4657b3c3946814a9eefa2bc6679b93b1471
Yogita Ganage <yogitaganage19@gmail.com>
2026-06-10 18:01:46 +0100

    test21

--- test.sql
+++ test.sql
@@ -1,5 +1,1 @@
-Updated WIP `session_patient_id` mapping to align with the attribute definition.
-
-The value is now built from `z_src_system_instance` + `client_id` from `silver_wip_activityheaderroledetail`, instead of using the person table ID.
-
-Validated against example care episode `573340`: `client_id = 304007`, expected `session_patient_id = WIP001304007`.
+test
