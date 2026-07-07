a91b16da6e0df3683cf05701682743be1b6a6a4d
Yogita Ganage <yogitaganage19@gmail.com>
2026-07-07 16:45:18 +0100

    d7

--- test.sql
+++ test.sql
@@ -1,20 +1,30 @@
 SELECT
-    ah.file_number,
-    ae.activity_date_time,
-    ast.description AS service_type,
-    srv.description AS service_activity,
-    atp.description AS activity_type,
-    CONCAT(ast.description, ' - ', srv.description, ' - ', atp.description) AS expected_cprod_name
-FROM silver_wip_activityentry ae
-LEFT JOIN silver_wip_activityheader ah
-    ON ae.activity_header_id = ah.id
-LEFT JOIN silver_wip_activityservice asv
-    ON ae.activity_service_id = asv.id
-LEFT JOIN silver_wip_servicetype ast
-    ON ah.service_type_id = ast.id
-LEFT JOIN silver_wip_service srv
-    ON asv.service_id = srv.id
-LEFT JOIN silver_wip_activitytype atp
-    ON ae.activity_type_id = atp.id
-WHERE TRIM(CAST(ah.file_number AS STRING)) IN ('451027', '306826')
-ORDER BY ah.file_number, ae.activity_date_time;
+    cprod_name,
+    cprod_src_id,
+    cprod_src_sys_inst_id
+FROM silver_rdm_care_product_add
+WHERE cprod_src_sys_inst_id = 'WIP001'
+  AND (
+        LOWER(cprod_name) LIKE '%bereaved emotional support%'
+     OR LOWER(cprod_name) LIKE '%emotional support%'
+     OR LOWER(cprod_name) LIKE '%repeat call%'
+     OR LOWER(cprod_name) LIKE '%assessment%'
+  )
+ORDER BY cprod_name;
+
+
+
+SELECT
+    cprod_id,
+    cprod_name,
+    cprod_src_id,
+    cprod_src_sys_inst_id
+FROM silver_rdm_care_product
+WHERE cprod_src_sys_inst_id = 'WIP001'
+  AND (
+        LOWER(cprod_name) LIKE '%bereaved emotional support%'
+     OR LOWER(cprod_name) LIKE '%emotional support%'
+     OR LOWER(cprod_name) LIKE '%repeat call%'
+     OR LOWER(cprod_name) LIKE '%assessment%'
+  )
+ORDER BY cprod_name;
