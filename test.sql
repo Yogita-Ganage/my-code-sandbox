85028dd6184d4c44858cf68999a3b57530b31378
Yogita Ganage <yogitaganage19@gmail.com>
2026-07-07 13:29:10 +0100

    d4

--- test.sql
+++ test.sql
@@ -1,12 +1,4 @@
-MAX(
-    CASE
-        WHEN LOWER(TRIM(ws.description)) IN (
-            'case raised in error',
-            'raised in error',
-            'bnssg - case raised in error',
-            'bnssg- case raised in error'
-        )
-        THEN 1
-        ELSE 0
-    END
-) AS has_case_raised_in_error
+                    'case raised in error',
+                    'raised in error',
+                    'bnssg- case raised in error',
+                    'bnssg - case raised in error'
