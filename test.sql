e263b657fbb3e780eb6181305ee915d3596ba8d7
Yogita Ganage <yogitaganage19@gmail.com>
2026-07-07 17:08:40 +0100

    d10

--- test.sql
+++ test.sql
@@ -1,4 +1,49 @@
 SELECT
+    ah.file_number,
+    ae.activity_date_time,
+    ast.description AS service_type,
+    srv.description AS service_activity,
+    atp.description AS activity_type,
+
+    CONCAT(ast.description, ' - ', srv.description, ' - ', atp.description) AS expected_cprod_name,
+
+    CONCAT('WIP001_', CONCAT(ast.description, ' - ', srv.description, ' - ', atp.description)) AS expected_cprod_src_id,
+
+    cp.cprod_id AS matched_cprod_id,
+    cp.cprod_name AS matched_cprod_name,
+    cp.cprod_src_id AS matched_cprod_src_id
+
+FROM silver_wip_activityentry ae
+
+LEFT JOIN silver_wip_activityheader ah
+    ON ae.activity_header_id = ah.id
+
+LEFT JOIN silver_wip_activityservice asv
+    ON ae.activity_service_id = asv.id
+
+LEFT JOIN silver_wip_servicetype ast
+    ON ah.service_type_id = ast.id
+
+LEFT JOIN silver_wip_service srv
+    ON asv.service_id = srv.id
+
+LEFT JOIN silver_wip_activitytype atp
+    ON ae.activity_type_id = atp.id
+
+LEFT JOIN silver_rdm_care_product cp
+    ON LOWER(TRIM(cp.cprod_src_id)) =
+       LOWER(TRIM(CONCAT('WIP001_', CONCAT(ast.description, ' - ', srv.description, ' - ', atp.description))))
+
+WHERE TRIM(CAST(ah.file_number AS STRING)) IN ('451027', '306826')
+
+ORDER BY ah.file_number, ae.activity_date_time;
+
+
+
+
+
+
+SELECT
     cprod_name,
     cprod_src_id,
     cprod_src_sys_inst_id
@@ -69,3 +114,9 @@ WHERE LOWER(a.cprod_name) LIKE '%bereaved emotional support%'
    OR LOWER(a.cprod_name) LIKE '%repeat call%'
    OR LOWER(a.cprod_name) LIKE '%assessment%'
 ORDER BY a.cprod_name;
+
+
+
+
+
+
