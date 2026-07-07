0ffa86a8fc4f2afa77293abaf9cbd24ae232a41c
Yogita Ganage <yogitaganage19@gmail.com>
2026-07-07 13:13:54 +0100

    d1

--- test.sql
+++ test.sql
@@ -1,1 +1,15 @@
-test
+SELECT
+    st.description,
+    LOWER(TRIM(st.description)) AS cleaned_status,
+    COUNT(*) AS row_count
+FROM silver_wip_activityentry ae
+LEFT JOIN silver_wip_activitystatus st
+    ON ae.activity_status_id = st.id
+WHERE LOWER(TRIM(st.description)) LIKE '%error%'
+   OR LOWER(TRIM(st.description)) LIKE '%raised%'
+   OR LOWER(TRIM(st.description)) LIKE '%cancel%'
+   OR LOWER(TRIM(st.description)) LIKE '%fail%'
+GROUP BY
+    st.description,
+    LOWER(TRIM(st.description))
+ORDER BY st.description;
