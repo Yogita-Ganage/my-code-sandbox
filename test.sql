b2bc024de61e8443ebdb161851a0e61db8071c5d
Yogita Ganage <yogitaganage19@gmail.com>
2026-07-08 13:34:34 +0100

    d22

--- test.sql
+++ test.sql
@@ -1,15 +1,16 @@
 SELECT
-    z_src_system_id,
-    COUNT(*) AS total_records,
-    SUM(CASE WHEN session_start_date_time IS NULL THEN 1 ELSE 0 END) AS blank_start_datetime_count,
-    SUM(CASE WHEN session_start_date_time IS NOT NULL THEN 1 ELSE 0 END) AS populated_start_datetime_count
-FROM silver_sessions
-WHERE z_src_system_id = 'SONE'
-GROUP BY z_src_system_id;
-
+    id,
+    id_organisation_source,
+    date_start,
+    date_end
+FROM silver_sone_srappointment
+WHERE date_start IS NOT NULL
+LIMIT 20;
 
 SELECT
     COUNT(*) AS total_appointments,
     SUM(CASE WHEN date_start IS NULL THEN 1 ELSE 0 END) AS blank_date_start_count,
-    SUM(CASE WHEN date_start IS NOT NULL THEN 1 ELSE 0 END) AS populated_date_start_count
+    SUM(CASE WHEN date_start IS NOT NULL THEN 1 ELSE 0 END) AS populated_date_start_count,
+    MIN(date_start) AS min_date_start,
+    MAX(date_start) AS max_date_start
 FROM silver_sone_srappointment;
