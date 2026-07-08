ccc28744f12049f1d4d5382965d4fda66fbc66b0
Yogita Ganage <yogitaganage19@gmail.com>
2026-07-08 13:49:14 +0100

    d23

--- test.sql
+++ test.sql
@@ -1,16 +1,2 @@
-SELECT
-    id,
-    id_organisation_source,
-    date_start,
-    date_end
-FROM silver_sone_srappointment
-WHERE date_start IS NOT NULL
-LIMIT 20;
-
-SELECT
-    COUNT(*) AS total_appointments,
-    SUM(CASE WHEN date_start IS NULL THEN 1 ELSE 0 END) AS blank_date_start_count,
-    SUM(CASE WHEN date_start IS NOT NULL THEN 1 ELSE 0 END) AS populated_date_start_count,
-    MIN(date_start) AS min_date_start,
-    MAX(date_start) AS max_date_start
-FROM silver_sone_srappointment;
+,to_timestamp(sra.date_start, 'dd MMM yyyy HH:mm:ss') AS session_start_date_time
+,to_timestamp(sra.date_end, 'dd MMM yyyy HH:mm:ss') AS session_end_date_time
