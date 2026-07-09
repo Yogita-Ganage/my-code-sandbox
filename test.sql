68a7b70054059e8db4a82ca599f14a6c30790d5d
Yogita Ganage <yogitaganage19@gmail.com>
2026-07-09 15:19:07 +0100

    e11

--- test.sql
+++ test.sql
@@ -1,24 +1,25 @@
 SELECT
     CONCAT('SONE', sra.id_organisation, sra.id) AS src_session_id,
-    CONCAT('SONE', sra.id_organisation, sra.id_referral_in) AS session_care_epi_id,
-    sra.id_patient,
-    CONCAT('SONE', sra.id_organisation, sra.id_patient) AS session_patient_id_from_appointment,
-    sra.*
+
+    CONCAT('SONE', sra.id_organisation, sra.id_referral_in) AS session_care_epi_id_from_appointment,
+
+    CONCAT('SONE', srri.id_organisation_source, srri.id) AS care_epi_id_from_referral,
+
+    sra.id_patient AS appointment_id_patient,
+
+    srri.id_patient AS referral_id_patient,
+
+    CONCAT('SONE', sra.id_organisation, sra.id_patient) AS current_session_patient_id_from_appointment,
+
+    CONCAT('SONE', srri.id_organisation_source, srri.id_patient) AS patient_id_from_referral
+
 FROM silver_sone_srappointment sra
+
+LEFT JOIN silver_sone_srreferralin srri
+    ON sra.id_referral_in = srri.id
+   AND sra.id_organisation = srri.id_organisation_source
+
 WHERE CONCAT('SONE', sra.id_organisation, sra.id) IN (
-    'SONEG4B9E343765868001',
-    'SONEG4B9E343763333367'
-);
-
-
-
-SELECT
-    CONCAT('SONE', id_organisation_source, id) AS care_epi_id,
-    id_patient,
-    CONCAT('SONE', id_organisation_source, id_patient) AS patient_id_from_referral,
-    *
-FROM silver_sone_srreferralin
-WHERE CONCAT('SONE', id_organisation_source, id) IN (
-    'SONEG4B9E76059032',
-    'SONEG4B9E76045740'
+    'इथे पहिला src_session_id टाक',
+    'इथे दुसरा src_session_id टाक'
 );
