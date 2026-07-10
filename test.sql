5488ee566a7aaa80bfeb2b08fedf05d0c8c4b05d
Yogita Ganage <yogitaganage19@gmail.com>
2026-07-10 12:47:48 +0100

    ee2

--- test.sql
+++ test.sql
@@ -1,1 +1,16 @@
-Peer review completed for MPB Care Episode Patient ID. The implemented logic CONCAT('MPB001', u.reference_id) matches the agreed definition, as reference_id contains the MPB short patient number. No issues found.
+SELECT
+    id AS referral_id,
+    id_organisation_source,
+    id_patient,
+    date_referral,
+    date_discharge
+FROM silver_sone_srreferralin
+WHERE id_organisation_source = 'G4B9E'
+  AND id IN (76346749, 76059032);
+
+
+
+  SELECT *
+FROM silver_sone_srpatient
+WHERE id_organisation_source = 'G4B9E'
+  AND id IN (66173613, 66164204);
