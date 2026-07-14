9e7c9141d43800d373aa7e0ee493952bf4e0e6c3
Yogita Ganage <yogitaganage19@gmail.com>
2026-07-14 13:54:25 +0100

    eeeee3

--- test.sql
+++ test.sql
@@ -1,20 +1,8 @@
 SELECT
     LOWER(TRIM(session_status_src_id)) AS session_status_src_id_normalised,
     COUNT(*) AS record_count
-FROM silver_rdm_session_status
+FROM silver_rdm_session_status_add
 WHERE session_status_src_id IS NOT NULL
 GROUP BY LOWER(TRIM(session_status_src_id))
 HAVING COUNT(*) > 1
 ORDER BY record_count DESC;
-
-
-SELECT *
-FROM silver_rdm_session_status
-WHERE LOWER(TRIM(session_status_src_id)) IN (
-    SELECT LOWER(TRIM(session_status_src_id))
-    FROM silver_rdm_session_status
-    WHERE session_status_src_id IS NOT NULL
-    GROUP BY LOWER(TRIM(session_status_src_id))
-    HAVING COUNT(*) > 1
-)
-ORDER BY LOWER(TRIM(session_status_src_id));
