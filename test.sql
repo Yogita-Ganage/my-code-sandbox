322d48bdb5f0e394875e759c2ad18d4c45a0363e
Yogita Ganage <yogitaganage19@gmail.com>
2026-07-16 11:57:04 +0100

    f8

--- test.sql
+++ test.sql
@@ -1,20 +1,5 @@
-%%sql
-
 SELECT
-    activity_header_id,
-    group_id,
-    group_desc,
-    raw_session_date,
-    clean_session_date,
-    parsed_form_ans_date
-FROM temp_wip_form_answer_parsed_date_lookup
-WHERE raw_session_date IN (
-    '29/08/1014',
-    '03/03/1016',
-    '17 December 1019',
-    '09/09/1021',
-    '1/12/1021',
-    '11/1/1023',
-    '21.2.1024'
-)
-ORDER BY raw_session_date;
+    COUNT(*) AS outside_range_dates
+FROM silver_form_answer
+WHERE form_ans_date < DATE '1899-12-30'
+   OR form_ans_date > DATE '9999-12-31';
