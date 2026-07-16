adca781491e8f39e84600db2a896cc305b93c6fe
Yogita Ganage <yogitaganage19@gmail.com>
2026-07-16 11:47:20 +0100

    f6

--- test.sql
+++ test.sql
@@ -1,13 +1,15 @@
-%%sql
-
 SELECT
     activity_header_id,
     group_id,
     group_desc,
     raw_session_date,
     clean_session_date,
-    parsed_form_ans_date
-FROM temp_wip_form_answer_parsed_date_lookup
-WHERE parsed_form_ans_date < DATE '1899-12-30'
-   OR parsed_form_ans_date > DATE '9999-12-31'
-ORDER BY parsed_form_ans_date;
+
+    CASE
+        WHEN COALESCE(v1_parsed_date, v2_fallback_parsed_date)
+             BETWEEN DATE '1899-12-30' AND DATE '9999-12-31'
+        THEN COALESCE(v1_parsed_date, v2_fallback_parsed_date)
+        ELSE NULL
+    END AS parsed_form_ans_date
+
+FROM parsed;
