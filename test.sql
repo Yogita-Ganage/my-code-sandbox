6ea9cc86fa4476c7f49069eac9b1725072e6fd7b
Yogita Ganage <yogitaganage19@gmail.com>
2026-07-16 11:31:12 +0100

    f5

--- test.sql
+++ test.sql
@@ -1,14 +1,13 @@
 %%sql
 
 SELECT
-    COUNT(*) AS total_lookup_rows,
-    COUNT(parsed_form_ans_date) AS parsed_rows,
-    SUM(
-        CASE
-            WHEN parsed_form_ans_date < DATE '1899-12-30'
-              OR parsed_form_ans_date > DATE '9999-12-31'
-            THEN 1
-            ELSE 0
-        END
-    ) AS outside_range_rows
-FROM temp_wip_form_answer_parsed_date_lookup;
+    activity_header_id,
+    group_id,
+    group_desc,
+    raw_session_date,
+    clean_session_date,
+    parsed_form_ans_date
+FROM temp_wip_form_answer_parsed_date_lookup
+WHERE parsed_form_ans_date < DATE '1899-12-30'
+   OR parsed_form_ans_date > DATE '9999-12-31'
+ORDER BY parsed_form_ans_date;
