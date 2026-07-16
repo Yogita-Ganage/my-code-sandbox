78c7a41bf047820db1436ae96c66bbb84fb29d23
Yogita Ganage <yogitaganage19@gmail.com>
2026-07-16 11:22:35 +0100

    f4

--- test.sql
+++ test.sql
@@ -1,24 +1,14 @@
 %%sql
 
-WITH sample_headers AS (
-    SELECT DISTINCT activity_header_id
-    FROM temp_silver_wip_activityheader_statistics
-    WHERE TRIM(LOWER(type_desc)) IN ('call date', 'session date')
-    LIMIT 5
-)
-
 SELECT
-    s.activity_header_id,
-    s.type_id,
-    s.type_desc,
-    s.group_id,
-    s.group_desc,
-    s.value_desc,
-    s.src_answer_desc
-FROM temp_silver_wip_activityheader_statistics s
-INNER JOIN sample_headers h
-    ON s.activity_header_id = h.activity_header_id
-ORDER BY
-    s.activity_header_id,
-    s.group_id,
-    s.type_id;
+    COUNT(*) AS total_lookup_rows,
+    COUNT(parsed_form_ans_date) AS parsed_rows,
+    SUM(
+        CASE
+            WHEN parsed_form_ans_date < DATE '1899-12-30'
+              OR parsed_form_ans_date > DATE '9999-12-31'
+            THEN 1
+            ELSE 0
+        END
+    ) AS outside_range_rows
+FROM temp_wip_form_answer_parsed_date_lookup;
