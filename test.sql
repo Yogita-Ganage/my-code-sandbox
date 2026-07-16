9ff35889d7e0ae0a7aa8694cf3d39ebd16baa650
Yogita Ganage <yogitaganage19@gmail.com>
2026-07-16 16:12:29 +0100

    f14

--- test.sql
+++ test.sql
@@ -1,33 +1,18 @@
-,CASE
-    WHEN TO_DATE(ar.created_at)
-         BETWEEN DATE '1899-12-30' AND DATE '9999-12-31'
-    THEN TO_DATE(ar.created_at)
-    ELSE CAST(NULL AS DATE)
-END AS form_ans_date
-
-
 %%sql
 
 SELECT
-    COUNT(*) AS total_rows,
-    COUNT(form_ans_date) AS populated_dates,
-    SUM(
-        CASE
-            WHEN form_ans_date < DATE '1899-12-30'
-              OR form_ans_date > DATE '9999-12-31'
-            THEN 1
-            ELSE 0
-        END
-    ) AS outside_range_dates
-FROM silver_form_answer_y;
-
-
-
-%%sql
-
-SELECT
+    form_ans_id,
     form_ans_care_epi_id,
+    form_ans_form_ques_id,
     form_ans_date,
-    form_ans_date_time
-FROM silver_form_answer_y
-WHERE form_ans_date IS NULL;
+    form_ans_date_time,
+    z_src_system_id,
+    z_src_system_instance
+FROM silver_form_answer
+WHERE z_src_system_id = 'MPB'
+  AND (
+        form_ans_date < DATE '1899-12-30'
+        OR form_ans_date > DATE '9999-12-31'
+      )
+ORDER BY form_ans_date
+LIMIT 100;
