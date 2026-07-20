448c5f6bc4319191e24c4505d5b424b92660137d
Yogita Ganage <yogitaganage19@gmail.com>
2026-07-20 11:57:12 +0100

    f18

--- test.sql
+++ test.sql
@@ -1,6 +1,6 @@
-CASE WHEN TO_DATE(TO_TIMESTAMP(ar.created_at,'yyyy-MM-dd''T''HH:mm:ss.SSS''Z'''))
+CASE WHEN TO_DATE(TO_TIMESTAMP(sc.date_event_recorded,'dd MMM yyyy HH:mm:ss'))
           BETWEEN DATE '1899-12-30' AND DATE '9999-12-31'
-     THEN TO_TIMESTAMP(ar.created_at,'yyyy-MM-dd''T''HH:mm:ss.SSS''Z''')
+     THEN TO_TIMESTAMP(sc.date_event_recorded,'dd MMM yyyy HH:mm:ss')
      ELSE NULL
 END AS form_ans_date_time
 
