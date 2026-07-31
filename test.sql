58bf9667fe636e55a398659ce089b40c9e2c95e3
Yogita Ganage <yogitaganage19@gmail.com>
2026-07-31 14:25:54 +0100

    f34

--- test.sql
+++ test.sql
@@ -1,7 +1,33 @@
-SELECT *
-FROM <care_product_add_test_table>
-WHERE LOWER(TRIM(cprod_src_id)) IN
-(
-    LOWER(TRIM('<first_unmatched_bridge_cprod_src_id>')),
-    LOWER(TRIM('<second_unmatched_bridge_cprod_src_id>'))
-);
+SELECT
+    s.id_rota,
+    s.id_organisation_source,
+    s.rota_slot_type,
+    r.rota_type,
+    s.blocked_slot,
+    CONCAT(
+        'SONE',
+        s.id_organisation_source,
+        '_',
+        LOWER(
+            CONCAT(
+                TRIM(s.rota_slot_type),
+                '_',
+                TRIM(r.rota_type)
+            )
+        )
+    ) AS generated_cprod_src_id
+FROM silver_sone_srrotaslot s
+LEFT JOIN silver_sone_srrota r
+    ON s.id_rota = r.id
+WHERE s.id_organisation_source = '00D1Z'
+  AND (
+        (
+            LOWER(TRIM(s.rota_slot_type)) = 'bromley referral screening'
+            AND LOWER(TRIM(r.rota_type)) = 'bromley mcats injection'
+        )
+        OR
+        (
+            LOWER(TRIM(s.rota_slot_type)) = 'blocked'
+            AND LOWER(TRIM(r.rota_type)) = 'bromley mcats'
+        )
+      );
