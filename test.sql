ef613677b052a34e32e03daa511962071afda156
Yogita Ganage <yogitaganage19@gmail.com>
2026-07-31 13:37:02 +0100

    f28

--- test.sql
+++ test.sql
@@ -1,8 +1,16 @@
 SELECT
-    s.session_patient_id,
-    s.session_cprod_src_id,
-    br.cprod_src_id AS bridge_cprod_src_id
-FROM silver_session_ytest s
-LEFT JOIN silver_sone_srrotaslot_bridging_to_srappointment br
-    ON CONCAT('SONE', s.session_src_sys_inst_id, s.session_src_id) = br.src_session_id
-WHERE s.session_patient_id = 'SONECOD1260771749';
+    sra.id,
+    CONCAT('SONE', sra.id_organisation, sra.id) AS src_session_id,
+    bridgetoapp.cprod_src_id,
+    rdmcp.cprod_src_name,
+    rdmcp.cprod_id
+FROM silver_sone_srappointment sra
+LEFT JOIN silver_sone_srrotaslot_bridging_to_srappointment bridgetoapp
+    ON CONCAT('SONE', sra.id_organisation, sra.id)
+       = bridgetoapp.src_session_id
+   AND sra.id_organisation = bridgetoapp.id_organisation_source
+LEFT JOIN silver_rdm_care_product rdmcp
+    ON LOWER(TRIM(rdmcp.cprod_src_id))
+       = LOWER(TRIM(bridgetoapp.cprod_src_id))
+WHERE CONCAT('SONE', sra.id_organisation, sra.id_patient)
+      = 'SONE00D1260771749';
