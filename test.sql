8c7c600724aaa3f85b4323a699faebf72c1a4ed8
Yogita Ganage <yogitaganage19@gmail.com>
2026-07-31 13:56:53 +0100

    f29

--- test.sql
+++ test.sql
@@ -1,16 +1,35 @@
 SELECT
-    sra.id,
-    CONCAT('SONE', sra.id_organisation, sra.id) AS src_session_id,
-    bridgetoapp.cprod_src_id,
-    rdmcp.cprod_src_name,
-    rdmcp.cprod_id
+    COUNT(*) AS total_records,
+    COUNT(rdmcp.cprod_src_id) AS matched_records,
+    COUNT(*) - COUNT(rdmcp.cprod_src_id) AS unmatched_records
 FROM silver_sone_srappointment sra
-LEFT JOIN silver_sone_srrotaslot_bridging_to_srappointment bridgetoapp
-    ON CONCAT('SONE', sra.id_organisation, sra.id)
-       = bridgetoapp.src_session_id
-   AND sra.id_organisation = bridgetoapp.id_organisation_source
+LEFT JOIN silver_sone_srrota srro
+    ON srro.id = sra.id_rota
+   AND srro.id_organisation_source = sra.id_organisation
+LEFT JOIN silver_sone_srrotaslot_bridging_to_srappointment br
+    ON CONCAT('SONE', sra.id_organisation, sra.id) = br.src_session_id
+   AND br.id_organisation_source = sra.id_organisation
 LEFT JOIN silver_rdm_care_product rdmcp
     ON LOWER(TRIM(rdmcp.cprod_src_id))
-       = LOWER(TRIM(bridgetoapp.cprod_src_id))
-WHERE CONCAT('SONE', sra.id_organisation, sra.id_patient)
-      = 'SONE00D1260771749';
+       = LOWER(TRIM(br.cprod_src_id));
+
+
+SELECT
+    COUNT(*) AS total_records,
+    COUNT(rdmcp.cprod_src_id) AS matched_records,
+    COUNT(*) - COUNT(rdmcp.cprod_src_id) AS unmatched_records
+FROM silver_sone_srappointment sra
+LEFT JOIN silver_sone_srrota srro
+    ON srro.id = sra.id_rota
+   AND srro.id_organisation_source = sra.id_organisation
+LEFT JOIN silver_sone_srrotaslot srslot
+    ON srslot.id_rota = sra.id_rota
+   AND srslot.id_organisation_source = sra.id_organisation
+LEFT JOIN silver_rdm_care_product rdmcp
+    ON LOWER(TRIM(rdmcp.cprod_src_id)) =
+       LOWER(TRIM(CONCAT(
+           'SONE',
+           sra.id_organisation,
+           '-',
+           LOWER(CONCAT(TRIM(srslot.rota_slot_type), '-', TRIM(srro.rota_type)))
+       )));
