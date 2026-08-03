0b70a64c4028d4bdc2144e50ba7db82bdaae55e6
Yogita Ganage <yogitaganage19@gmail.com>
2026-08-03 16:26:19 +0100

    f38

--- test.sql
+++ test.sql
@@ -1,1 +1,5 @@
-test
+SELECT
+    COUNT(*) AS matched_rows
+FROM silver_wip_activityheader ah
+JOIN silver_staging_completion_status_conformed compconf
+ON CONCAT('WIP001', ah.file_number) = compconf.session_care_epi_id;
