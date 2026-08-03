2462ef852aee52201d3957e83f0e22fc7542f42b
Yogita Ganage <yogitaganage19@gmail.com>
2026-08-03 16:34:58 +0100

    f39

--- test.sql
+++ test.sql
@@ -1,5 +1,7 @@
 SELECT
-    COUNT(*) AS matched_rows
+    COUNT(*) AS total,
+    COUNT(compconf.session_care_epi_id) AS matched,
+    COUNT(*) - COUNT(compconf.session_care_epi_id) AS unmatched
 FROM silver_wip_activityheader ah
-JOIN silver_staging_completion_status_conformed compconf
-ON CONCAT('WIP001', ah.file_number) = compconf.session_care_epi_id;
+LEFT JOIN silver_staging_completion_status_conformed compconf
+    ON CONCAT('WIP001', ah.file_number) = compconf.session_care_epi_id;
