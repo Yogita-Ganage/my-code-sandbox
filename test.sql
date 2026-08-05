4521dde0f5b5c48619a92c499a07f4f16a602950
Yogita Ganage <yogitaganage19@gmail.com>
2026-08-05 13:11:50 +0100

    f52

--- test.sql
+++ test.sql
@@ -1,13 +1,5 @@
-SELECT
-    user_id,
-    COUNT(*) AS row_count
-FROM silver_staging_mpb_pathway
-WHERE change_num_desc = 1
-GROUP BY user_id
-HAVING COUNT(*) > 1
-ORDER BY row_count DESC;
-
 SELECT *
 FROM silver_staging_mpb_pathway
 WHERE user_id = '9a1f508e-9624-43c5-86ff-7ebc5d8d289e'
+  AND change_num_desc = 1
 ORDER BY datetime DESC;
