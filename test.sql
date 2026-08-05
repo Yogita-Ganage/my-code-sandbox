6b02dad3b3a836ab69ce0250d6f064832cbf735d
Yogita Ganage <yogitaganage19@gmail.com>
2026-08-05 13:41:24 +0100

    f60

--- test.sql
+++ test.sql
@@ -1,24 +1,14 @@
 SELECT
-    u.id AS user_id,
-    ten.client_type,
-    rserv.service_src_name,
-    rserv.service_id,
-    COUNT(*) AS match_count
-FROM silver_drj_users u
-
-LEFT JOIN silver_drj_tenancies ten
-    ON ten.id = u.tenancy_id
-
-LEFT JOIN silver_rdm_service rserv
-    ON rserv.src_sys_inst_id = 'MPB001'
-   AND TRIM(LOWER(rserv.service_src_name))
-       = TRIM(LOWER(ten.client_type))
-
-WHERE u.profile_type = 'user'
-  AND u.id = '9a1f508e-9624-43c5-86ff-7ebc5d8d289e'
-
+    id,
+    reference_id,
+    episode_number,
+    tenancy_id,
+    COUNT(*) AS row_count
+FROM silver_drj_users
+WHERE profile_type = 'user'
+  AND id = '9a1f508e-9624-43c5-86ff-7ebc5d8d289e'
 GROUP BY
-    u.id,
-    ten.client_type,
-    rserv.service_src_name,
-    rserv.service_id;
+    id,
+    reference_id,
+    episode_number,
+    tenancy_id;
