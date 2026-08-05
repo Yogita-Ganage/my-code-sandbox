d6bcf8aecdd39d2b27c9307374580d60d33a850f
Yogita Ganage <yogitaganage19@gmail.com>
2026-08-05 13:28:07 +0100

    f58

--- test.sql
+++ test.sql
@@ -1,14 +1,12 @@
 SELECT
-    session_care_epi_id,
+    title,
     COUNT(*) AS row_count
-FROM (
-    SELECT
-        session_care_epi_id,
-        MAX(session_attended_flag) AS session_attended_flag,
-        MAX(session_bkd_in_future_flag) AS session_bkd_in_future_flag
-    FROM silver_sessions
-    GROUP BY session_care_epi_id
-) session_flags_comb
-WHERE session_care_epi_id =
-      'MPB0019a1f508e-9624-43c5-86ff-7ebc5d8d289e'
-GROUP BY session_care_epi_id;
+FROM silver_drj_anamnesises_epicrisis
+WHERE user_id = '9a1f508e-9624-43c5-86ff-7ebc5d8d289e'
+  AND title IN (
+      'Discharge Reason',
+      'Discharge Status',
+      'Treatment Type'
+  )
+GROUP BY title
+ORDER BY title;
