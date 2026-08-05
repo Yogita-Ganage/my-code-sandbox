0664193fe5ed9269b91fa787b95f82d4cb845631
Yogita Ganage <yogitaganage19@gmail.com>
2026-08-05 13:14:35 +0100

    f53

--- test.sql
+++ test.sql
@@ -1,5 +1,18 @@
-SELECT *
-FROM silver_staging_mpb_pathway
-WHERE user_id = '9a1f508e-9624-43c5-86ff-7ebc5d8d289e'
-  AND change_num_desc = 1
-ORDER BY datetime DESC;
+SELECT
+    u.id AS user_id,
+    u.tenancy_id,
+    CONCAT('MPB', ten.id) AS expected_contr_id,
+    con.contr_id,
+    COUNT(*) AS contract_row_count
+FROM silver_drj_users u
+LEFT JOIN silver_drj_tenancies ten
+    ON ten.id = u.tenancy_id
+LEFT JOIN silver_contract con
+    ON con.contr_id = CONCAT('MPB', ten.id)
+WHERE u.profile_type = 'user'
+  AND u.id = '9a1f508e-9624-43c5-86ff-7ebc5d8d289e'
+GROUP BY
+    u.id,
+    u.tenancy_id,
+    CONCAT('MPB', ten.id),
+    con.contr_id;
