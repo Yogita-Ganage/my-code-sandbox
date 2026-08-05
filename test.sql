69d32ede7552e722e45a341cdf4818d123318f2d
Yogita Ganage <yogitaganage19@gmail.com>
2026-08-05 12:22:29 +0100

    f46

--- test.sql
+++ test.sql
@@ -1,1 +1,7 @@
-test
+SELECT
+    u.id,
+    COUNT(DISTINCT CONCAT(u.referral_id,'-',u.episode_number)) AS episode_count
+FROM silver_drj_users u
+WHERE profile_type = 'user'
+GROUP BY u.id
+HAVING COUNT(DISTINCT CONCAT(u.referral_id,'-',u.episode_number)) > 1;
