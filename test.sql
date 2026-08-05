5f74d379afa91c4ad78a3db65a163fc2bb30b683
Yogita Ganage <yogitaganage19@gmail.com>
2026-08-05 13:19:42 +0100

    f55

--- test.sql
+++ test.sql
@@ -1,7 +1,7 @@
 SELECT
     u.id AS user_id,
-    serv.value AS source_service_value,
-    pserv.care_epi_primary_service_requested,
+    src.value AS source_referral_value,
+    refs.care_epi_referral_source,
     COUNT(*) AS match_count
 FROM silver_drj_users u
 
@@ -16,19 +16,20 @@ LEFT JOIN (
             ORDER BY updated_at DESC
         ) AS rowno
     FROM silver_drj_anamnesises_epicrisis
-    WHERE title = 'Treatment Type'
-) serv
-    ON serv.user_id = u.id
-   AND serv.rowno = 1
+    WHERE title = 'Referral Source'
+) src
+    ON src.user_id = u.id
+   AND src.rowno = 1
 
-LEFT JOIN silver_rdm_care_episode_primary_service pserv
-    ON pserv.care_epi_primary_service_requested = serv.value
-   AND pserv.z_src_system_id = 'MPB'
+LEFT JOIN silver_rdm_referral_source refs
+    ON TRIM(LOWER(refs.care_epi_referral_source)) =
+       TRIM(LOWER(src.value))
+   AND refs.z_src_system_id = 'MPB'
 
 WHERE u.profile_type = 'user'
   AND u.id = '9a1f508e-9624-43c5-86ff-7ebc5d8d289e'
 
 GROUP BY
     u.id,
-    serv.value,
-    pserv.care_epi_primary_service_requested;
+    src.value,
+    refs.care_epi_referral_source;
