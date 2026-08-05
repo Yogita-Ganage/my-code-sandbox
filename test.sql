bd012a41a0597a54197b59d23069d7361b1ad842
Yogita Ganage <yogitaganage19@gmail.com>
2026-08-05 12:51:04 +0100

    f48

--- test.sql
+++ test.sql
@@ -1,15 +1,10 @@
-SELECT
-    care_epi_id,
-    care_epi_number,
-    COUNT(*) AS row_count
-FROM silver_care_episode_ytest
-WHERE z_src_system_id = 'MPB'
-GROUP BY
-    care_epi_id,
-    care_epi_number
-HAVING COUNT(*) > 1
-ORDER BY row_count DESC;
-
 SELECT *
-FROM silver_care_episode_ytest
-WHERE care_epi_id = 'MPB0019a1f508e-9624-43c5-86ff-7ebc5d8d289e';
+FROM silver_rdm_source_systems
+WHERE src_sys_inst_id = 'MPB001';
+
+SELECT
+    src_sys_inst_id,
+    COUNT(*)
+FROM silver_rdm_source_systems
+GROUP BY src_sys_inst_id
+HAVING COUNT(*) > 1;
