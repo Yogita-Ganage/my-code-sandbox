115ab1c4af5d724b23f89a865a5330c71bf84359
Yogita Ganage <yogitaganage19@gmail.com>
2026-08-05 14:04:57 +0100

    f63

--- test.sql
+++ test.sql
@@ -1,4 +1,11 @@
-SELECT *
+SELECT
+    care_epi_pathway,
+    care_epi_pathway_conformed,
+    z_record_created_date_time,
+    z_record_modified_date_time,
+    z_record_created_by_user,
+    z_record_modified_by_user
 FROM silver_rdm_pathway
 WHERE z_src_system_id = 'MPB'
-  AND TRIM(LOWER(care_epi_pathway)) = 'high intensity (cbt)';
+  AND TRIM(LOWER(care_epi_pathway)) = 'high intensity (cbt)'
+ORDER BY z_record_modified_date_time DESC;
