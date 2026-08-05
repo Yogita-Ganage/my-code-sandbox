1eeba3b60376a6790215651ee903ed4470bcad2d
Yogita Ganage <yogitaganage19@gmail.com>
2026-08-05 13:25:07 +0100

    f57

--- test.sql
+++ test.sql
@@ -1,7 +1,14 @@
 SELECT
     session_care_epi_id,
     COUNT(*) AS row_count
-FROM silver_staging_completion_status_conformed
+FROM (
+    SELECT
+        session_care_epi_id,
+        MAX(session_attended_flag) AS session_attended_flag,
+        MAX(session_bkd_in_future_flag) AS session_bkd_in_future_flag
+    FROM silver_sessions
+    GROUP BY session_care_epi_id
+) session_flags_comb
 WHERE session_care_epi_id =
-      'MPB9a1f508e-9624-43c5-86ff-7ebc5d8d289e'
+      'MPB0019a1f508e-9624-43c5-86ff-7ebc5d8d289e'
 GROUP BY session_care_epi_id;
