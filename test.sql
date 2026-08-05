3c7785c16ff9048d39b22e1f10477bc909f9f4a4
Yogita Ganage <yogitaganage19@gmail.com>
2026-08-05 13:55:06 +0100

    f62

--- test.sql
+++ test.sql
@@ -1,21 +1,4 @@
-SELECT
-    pw.user_id,
-    pw.pathway,
-    rdmpwc.care_epi_pathway,
-    rdmpwc.z_src_system_id,
-    COUNT(*) AS match_count
-FROM silver_staging_mpb_pathway pw
-
-LEFT JOIN silver_rdm_pathway rdmpwc
-    ON TRIM(LOWER(rdmpwc.care_epi_pathway))
-       = TRIM(LOWER(pw.pathway))
-   AND rdmpwc.z_src_system_id = 'MPB'
-
-WHERE pw.user_id = '9a1f508e-9624-43c5-86ff-7ebc5d8d289e'
-  AND pw.change_num_desc = 1
-
-GROUP BY
-    pw.user_id,
-    pw.pathway,
-    rdmpwc.care_epi_pathway,
-    rdmpwc.z_src_system_id;
+SELECT *
+FROM silver_rdm_pathway
+WHERE z_src_system_id = 'MPB'
+  AND TRIM(LOWER(care_epi_pathway)) = 'high intensity (cbt)';
