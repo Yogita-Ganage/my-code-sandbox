178a0929923ed288f52644db2a3d6457453ee803
Yogita Ganage <yogitaganage19@gmail.com>
2026-08-05 13:49:44 +0100

    f61

--- test.sql
+++ test.sql
@@ -1,14 +1,21 @@
 SELECT
-    id,
-    reference_id,
-    episode_number,
-    tenancy_id,
-    COUNT(*) AS row_count
-FROM silver_drj_users
-WHERE profile_type = 'user'
-  AND id = '9a1f508e-9624-43c5-86ff-7ebc5d8d289e'
+    pw.user_id,
+    pw.pathway,
+    rdmpwc.care_epi_pathway,
+    rdmpwc.z_src_system_id,
+    COUNT(*) AS match_count
+FROM silver_staging_mpb_pathway pw
+
+LEFT JOIN silver_rdm_pathway rdmpwc
+    ON TRIM(LOWER(rdmpwc.care_epi_pathway))
+       = TRIM(LOWER(pw.pathway))
+   AND rdmpwc.z_src_system_id = 'MPB'
+
+WHERE pw.user_id = '9a1f508e-9624-43c5-86ff-7ebc5d8d289e'
+  AND pw.change_num_desc = 1
+
 GROUP BY
-    id,
-    reference_id,
-    episode_number,
-    tenancy_id;
+    pw.user_id,
+    pw.pathway,
+    rdmpwc.care_epi_pathway,
+    rdmpwc.z_src_system_id;
