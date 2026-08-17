5dc3f15299f82edc097f17c851a4ac7292708e62
Yogita Ganage <yogitaganage19@gmail.com>
2026-08-17 15:52:57 +0100

    g1

--- test.sql
+++ test.sql
@@ -1,25 +1,8 @@
-%%sql
+config_df = spark.read.options(header=True, delimiter=",").csv("Files/bronze_Control_file_prod.csv")
 
-SELECT DISTINCT
-    CONCAT('MPB001', ar.user_id) AS form_ans_care_epi_id,
-    ar.created_at AS source_created_at,
-
-    TO_TIMESTAMP(
-        ar.created_at,
-        'yyyy-MM-dd''T''HH:mm:ss.SSS''Z'''
-    ) AS current_timestamp,
-
-    FROM_UTC_TIMESTAMP(
-        TO_TIMESTAMP(
-            ar.created_at,
-            'yyyy-MM-dd''T''HH:mm:ss.SSS''Z'''
-        ),
-        'Europe/London'
-    ) AS uk_local_timestamp
-
-FROM silver_drj_assessment_results ar
-
-WHERE CONCAT('MPB001', ar.user_id)
-      = '<तू आत्ता वापरलेला exact UAT CARE EPI ID>'
-
-ORDER BY source_created_at;
+display(
+    config_df.filter(
+        (config_df.sourcesystem == "drj") &
+        (config_df.tablename == "tenancies")
+    )
+)
