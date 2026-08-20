a92a59154af91e85975241c20ee9fcf6b76f6ed2
Yogita Ganage <yogitaganage19@gmail.com>
2026-08-20 13:11:45 +0100

    f4

--- test.sql
+++ test.sql
@@ -1,9 +1,21 @@
-config_df = spark.read.options(header=True, delimiter=",").csv("Files/bronze_Control_file_prod.csv")
-
-display(
-    config_df.filter(
-        (config_df.sourcesystem == "drj") &
-        (config_df.tablename == "tenancies")
-    )
+df_test = (
+    spark.read
+    .option("header", True)
+    .option("delimiter", "|")
+    .csv("Files/landing/DRJ/Tenancies_20260820/2026/08/20/Tenancies_202608200205.csv")
+    .limit(20)
 )
-ffx
+
+df_test.write \
+    .format("delta") \
+    .mode("overwrite") \
+    .option("overwriteSchema", "true") \
+    .saveAsTable("zz_test_bronze_drj_tenancies")
+
+
+
+    SELECT
+    id,
+    clientType,
+    invoicePrefix
+FROM zz_test_bronze_drj_tenancies;
