15ff71b1bfd5e6a83b896d42806405e740b41729
Yogita Ganage <yogitaganage19@gmail.com>
2026-08-24 13:42:05 +0100

    f8

--- test.sql
+++ test.sql
@@ -1,1 +1,19 @@
-test
+CASE
+    WHEN TO_DATE(
+        TO_TIMESTAMP(
+            ar.created_at,
+            'yyyy-MM-dd''T''HH:mm:ss.SSS''Z'''
+        )
+    )
+    BETWEEN DATE '1899-12-30' AND DATE '9999-12-31'
+
+    THEN FROM_UTC_TIMESTAMP(
+        TO_TIMESTAMP(
+            ar.created_at,
+            'yyyy-MM-dd''T''HH:mm:ss.SSS''Z'''
+        ),
+        'Europe/London'
+    )
+
+    ELSE NULL
+END AS form_ans_date_time
