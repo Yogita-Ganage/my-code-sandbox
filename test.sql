32f1dfe10b867d197a0f85b1dcb230ced0ea881d
Yogita Ganage <yogitaganage19@gmail.com>
2026-08-25 15:41:45 +0100

    f11

--- test.sql
+++ test.sql
@@ -1,1 +1,8 @@
-Updated the WIP `form_ans_date_time` implementation as per the revised definition. The field now uses `parsed_form_ans_date` cast as a timestamp, resulting in the answer date with the time defaulted to `00:00:00` instead of NULL.
+SELECT DISTINCT
+    at.id AS del_meth_src_id,
+    at.name AS del_meth_src_name,
+    'MPB001' AS z_src_system_instance
+FROM silver_drj_appointments a
+INNER JOIN silver_drj_appointment_types at
+    ON a.appointment_type_id = at.id
+WHERE at.id IS NOT NULL
