1d7bdab78564cac541938437b19cdf639a7896d2
Yogita Ganage <yogitaganage19@gmail.com>
2026-08-26 12:29:00 +0100

    f16

--- test.sql
+++ test.sql
@@ -26,3 +26,13 @@ LEFT JOIN silver_wip_activitytype at
 WHERE st.id IS NULL
    OR s.id IS NULL
    OR at.id IS NULL;
+
+
+
+
+SELECT
+    COUNT(*) AS total_rows,
+    SUM(CASE WHEN ah.service_type_id IS NULL THEN 1 ELSE 0 END) AS null_service_type_rows
+FROM silver_wip_activityentry ae
+LEFT JOIN silver_wip_activityheader ah
+    ON ae.activity_header_id = ah.id;
