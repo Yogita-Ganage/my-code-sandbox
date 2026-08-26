a3c2a7c854f84bc44ccf1334a30df099b4599a3d
Yogita Ganage <yogitaganage19@gmail.com>
2026-08-26 16:59:57 +0100

    f22

--- test.sql
+++ test.sql
@@ -1,13 +1,17 @@
 SELECT DISTINCT
-    rota_slot_type
-FROM silver_sone_srrotaslot_bridging_to_srappointment
-WHERE rota_slot_type IS NOT NULL
-ORDER BY rota_slot_type;
-
-SELECT
-    rota_slot_type,
-    COUNT(*) AS record_count
-FROM silver_sone_srrotaslot_bridging_to_srappointment
-WHERE rota_slot_type IS NOT NULL
-GROUP BY rota_slot_type
-ORDER BY rota_slot_type;
+    s.rota_slot_type,
+    r.rota_type,
+    CONCAT(TRIM(s.rota_slot_type), ' - ', TRIM(r.rota_type)) AS del_meth_src_name
+FROM silver_sone_srrotaslot s
+LEFT JOIN silver_sone_srrota r
+    ON s.id_rota = r.id
+    AND s.id_organisation_source = r.id_organisation_source
+WHERE s.rota_slot_type IS NOT NULL
+  AND r.rota_type IS NOT NULL
+  AND (
+       LOWER(CONCAT(s.rota_slot_type, ' ', r.rota_type)) LIKE '%face%'
+    OR LOWER(CONCAT(s.rota_slot_type, ' ', r.rota_type)) LIKE '%f2f%'
+    OR LOWER(CONCAT(s.rota_slot_type, ' ', r.rota_type)) LIKE '%telephone%'
+    OR LOWER(CONCAT(s.rota_slot_type, ' ', r.rota_type)) LIKE '%video%'
+  )
+ORDER BY del_meth_src_name;
