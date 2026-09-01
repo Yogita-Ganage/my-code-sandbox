bb8aa661394c85ea121ebfe4586d2f2a9c19856a
Yogita Ganage <yogitaganage19@gmail.com>
2026-09-01 17:09:21 +0100

    g10

--- test.sql
+++ test.sql
@@ -1,6 +1,22 @@
-SELECT *
+SELECT DISTINCT
+    src_session_id,
+    id_organisation_source,
+    rota_slot_type,
+    rota_type
 FROM silver_sone_srrotaslot_bridging_to_srappointment
-WHERE src_session_id IN (
-    'SONE00D1Z34566831071',
-    'SONE00D1Z34466326203'
-);
+WHERE src_session_id LIKE '%34566831071%'
+   OR src_session_id LIKE '%34466326203%';
+
+
+
+SELECT DISTINCT
+    src_session_id,
+    id_organisation_source,
+    rota_slot_type,
+    rota_type
+FROM silver_sone_srrotaslot_bridging_to_srappointment
+WHERE id_organisation_source = '00D1Z'
+  AND (
+        LOWER(rota_slot_type) LIKE '%initial consultation%'
+        OR LOWER(rota_type) LIKE '%bromley physio%'
+      );
