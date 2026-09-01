13d5a0e01c2b679a35fe5d3fab25bcdd256e82c4
Yogita Ganage <yogitaganage19@gmail.com>
2026-09-01 16:23:56 +0100

    g5

--- test.sql
+++ test.sql
@@ -1,17 +1,58 @@
+WITH base AS (
+    SELECT DISTINCT
+        srref.id AS referral_id,
+        sra.id AS appointment_id,
+        sra.id_organisation_source,
+        bridgetoapp.rota_slot_type,
+        bridgetoapp.rota_type
+    FROM silver_sone_srappointment sra
+
+    LEFT JOIN silver_sone_srreferralin srref
+        ON srref.id = sra.id_referral_in
+
+    LEFT JOIN silver_sone_srrotaslot_bridging_to_srappointment bridgetoapp
+        ON CONCAT('SONE', sra.id_organisation, sra.id) = bridgetoapp.src_session_id
+        AND sra.id_organisation = bridgetoapp.id_organisation_source
+
+    WHERE srref.id = 75902966
+),
+
+mapped AS (
+    SELECT *,
+        CASE
+            WHEN LOWER(TRIM(rota_slot_type)) LIKE '%f2f%'
+              OR LOWER(TRIM(rota_slot_type)) LIKE '%face to face%'
+                THEN 'Face to Face'
+
+            WHEN LOWER(TRIM(rota_slot_type)) LIKE '%remote%'
+              OR LOWER(TRIM(rota_slot_type)) LIKE '%telephone%'
+                THEN 'Telephone'
+
+            WHEN LOWER(TRIM(rota_slot_type)) LIKE '%video%'
+                THEN 'Video'
+
+            ELSE 'Unmapped'
+        END AS derived_delivery_method
+    FROM base
+)
+
 SELECT
-    sra.id AS appointment_id,
-    sra.id_referral_in,
-    sra.id_organisation,
-    sra.id_rota,
-    sra.rota_name,
-    sra.rota_type,
-    bridgetoapp.*
-FROM silver_sone_srappointment sra
+    referral_id,
+    appointment_id,
+    rota_slot_type,
+    rota_type,
+    derived_delivery_method,
 
-LEFT JOIN silver_sone_srrotaslot_bridging_to_srappointment bridgetoapp
-    ON CONCAT('SONE', sra.id_organisation, sra.id)
-       = bridgetoapp.src_session_id
-    AND sra.id_organisation
-       = bridgetoapp.id_organisation_source
+    CONCAT(
+        derived_delivery_method,
+        ' - ',
+        TRIM(rota_type)
+    ) AS del_meth_src_name,
 
-WHERE sra.id IN (34566831071, 34466326203);
+    CONCAT(
+        'SONE',
+        id_organisation_source
+    ) AS del_meth_src_sys_inst_id
+
+FROM mapped
+ORDER BY appointment_id;
