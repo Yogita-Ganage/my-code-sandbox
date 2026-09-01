640eee085c553c8ffe9965f17e63696a2ec85cb3
Yogita Ganage <yogitaganage19@gmail.com>
2026-09-01 15:16:21 +0100

    g3

--- test.sql
+++ test.sql
@@ -1,44 +1,106 @@
-SELECT
-    TRIM(rota_slot_type) AS rota_slot_type,
+%%sql
 
-    CASE
-        WHEN LOWER(TRIM(rota_slot_type)) LIKE '%f2f%'
-          OR LOWER(TRIM(rota_slot_type)) LIKE '%face to face%'
-            THEN 'Face to Face'
+DROP TABLE IF EXISTS silver_rdm_delivery_method_add;
 
-        WHEN LOWER(TRIM(rota_slot_type)) LIKE '%telephone%'
-          OR LOWER(TRIM(rota_slot_type)) LIKE '%remote%'
-            THEN 'Telephone'
+CREATE TABLE silver_rdm_delivery_method_add AS
 
-        WHEN LOWER(TRIM(rota_slot_type)) LIKE '%video%'
-            THEN 'Video'
+WITH sone_source AS
+(
+    SELECT DISTINCT
 
-        ELSE 'Unmapped'
-    END AS derived_delivery_method,
+        /* Delivery Method Source ID */
+        CONCAT(
+            'SONE',
+            sra.id_organisation,
+            '_',
+            LOWER(
+                CONCAT(
+                    CASE
+                        WHEN LOWER(TRIM(bridgetoapp.rota_slot_type))
+                             = 'initial consultation f2f'
+                            THEN 'Face to Face'
 
-    COUNT(*) AS record_count
+                        WHEN LOWER(TRIM(bridgetoapp.rota_slot_type))
+                             = 'initial consultation remote'
+                            THEN 'Telephone'
+                    END,
+                    ' - ',
+                    TRIM(bridgetoapp.rota_type)
+                )
+            )
+        ) AS del_meth_src_id,
 
-FROM silver_sone_srrotaslot_bridging_to_srappointment
 
-WHERE rota_slot_type IS NOT NULL
+        /* Delivery Method Source System Instance ID */
+        CONCAT(
+            'SONE',
+            sra.id_organisation
+        ) AS del_meth_src_sys_inst_id,
 
-GROUP BY
-    TRIM(rota_slot_type),
-    CASE
-        WHEN LOWER(TRIM(rota_slot_type)) LIKE '%f2f%'
-          OR LOWER(TRIM(rota_slot_type)) LIKE '%face to face%'
-            THEN 'Face to Face'
 
-        WHEN LOWER(TRIM(rota_slot_type)) LIKE '%telephone%'
-          OR LOWER(TRIM(rota_slot_type)) LIKE '%remote%'
-            THEN 'Telephone'
+        /* Delivery Method Source Name */
+        CONCAT(
+            CASE
+                WHEN LOWER(TRIM(bridgetoapp.rota_slot_type))
+                     = 'initial consultation f2f'
+                    THEN 'Face to Face'
 
-        WHEN LOWER(TRIM(rota_slot_type)) LIKE '%video%'
-            THEN 'Video'
+                WHEN LOWER(TRIM(bridgetoapp.rota_slot_type))
+                     = 'initial consultation remote'
+                    THEN 'Telephone'
+            END,
+            ' - ',
+            TRIM(bridgetoapp.rota_type)
+        ) AS del_meth_src_name
 
-        ELSE 'Unmapped'
-    END
 
-ORDER BY
-    derived_delivery_method,
-    rota_slot_type;
+    FROM silver_sone_srappointment sra
+
+    LEFT JOIN silver_sone_srrotaslot_bridging_to_srappointment bridgetoapp
+        ON CONCAT(
+               'SONE',
+               sra.id_organisation,
+               sra.id
+           ) = bridgetoapp.src_session_id
+
+       AND sra.id_organisation
+           = bridgetoapp.id_organisation_source
+
+
+    /* Only the two exact mappings supplied in the definition */
+    WHERE LOWER(TRIM(bridgetoapp.rota_slot_type)) IN
+          (
+              'initial consultation f2f',
+              'initial consultation remote'
+          )
+
+      AND bridgetoapp.rota_type IS NOT NULL
+)
+
+
+SELECT DISTINCT
+    s.del_meth_src_id,
+    s.del_meth_src_sys_inst_id,
+    s.del_meth_src_name
+
+FROM sone_source s
+
+LEFT JOIN silver_rdm_delivery_method rdm
+    ON LOWER(TRIM(rdm.del_meth_src_name))
+       = LOWER(TRIM(s.del_meth_src_name))
+
+   AND LOWER(TRIM(rdm.del_meth_src_sys_inst_id))
+       = LOWER(TRIM(s.del_meth_src_sys_inst_id))
+
+WHERE rdm.del_meth_src_name IS NULL;
+
+
+
+
+
+%%sql
+
+SELECT *
+FROM silver_rdm_delivery_method_add
+ORDER BY del_meth_src_sys_inst_id,
+         del_meth_src_name;
