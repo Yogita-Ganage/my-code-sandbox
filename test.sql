bf0fd4145e8e2175d5eb14c306d59ad2e217bfef
Yogita Ganage <yogitaganage19@gmail.com>
2026-09-01 12:53:28 +0100

    f25

--- test.sql
+++ test.sql
@@ -1,1 +1,10 @@
-test
+SELECT
+    id,
+    referral_code,
+    id_referral_local,
+    id_organisation,
+    id_organisation_source
+FROM silver_sone_srreferralin
+WHERE CAST(id AS STRING) = '75902966'
+   OR CAST(referral_code AS STRING) = '75902966'
+   OR CAST(id_referral_local AS STRING) = '75902966';
