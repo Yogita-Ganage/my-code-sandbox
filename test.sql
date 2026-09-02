3e7cf20b7efde6c9787cb0ff28f0863b065b6649
Yogita Ganage <yogitaganage19@gmail.com>
2026-09-02 11:59:49 +0100

    g15

--- test.sql
+++ test.sql
@@ -19,10 +19,44 @@ bridge_source AS (
       AND rota_type IS NOT NULL
 )
 
-SELECT 'Direct Source' AS source, COUNT(*) AS record_count
+SELECT *
 FROM direct_source
 
-UNION ALL
+EXCEPT
 
-SELECT 'Bridge Source' AS source, COUNT(*) AS record_count
+SELECT *
 FROM bridge_source;
+
+
+
+
+
+
+WITH direct_source AS (
+    SELECT DISTINCT
+        TRIM(s.rota_slot_type) AS rota_slot_type,
+        TRIM(r.rota_type) AS rota_type
+    FROM silver_sone_srrotaslot s
+    INNER JOIN silver_sone_srrota r
+        ON s.id_rota = r.id
+       AND s.id_organisation_source = r.id_organisation_source
+    WHERE s.rota_slot_type IS NOT NULL
+      AND r.rota_type IS NOT NULL
+),
+
+bridge_source AS (
+    SELECT DISTINCT
+        TRIM(rota_slot_type) AS rota_slot_type,
+        TRIM(rota_type) AS rota_type
+    FROM silver_sone_srrotaslot_bridging_to_srappointment
+    WHERE rota_slot_type IS NOT NULL
+      AND rota_type IS NOT NULL
+)
+
+SELECT *
+FROM bridge_source
+
+EXCEPT
+
+SELECT *
+FROM direct_source;
