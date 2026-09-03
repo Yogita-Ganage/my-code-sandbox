ede81ad1ef59dbe0c5f74dbc81a158a6ebbfa9bf
Yogita Ganage <yogitaganage19@gmail.com>
2026-09-03 11:54:56 +0100

    g30

--- test.sql
+++ test.sql
@@ -1,44 +1,57 @@
-SELECT
-    SerT.id AS service_type_id,
-    srv.id AS service_id,
-    atyp.id AS activity_type_id,
-    COUNT(*) AS record_count
+SELECT DISTINCT
+    st.id AS service_type_id,
+    s.id AS service_id,
+    at.id AS activity_type_id,
+
+    CONCAT(
+        'WIP001_',
+        CAST(st.id AS STRING),
+        '_',
+        CAST(s.id AS STRING),
+        '_',
+        CAST(at.id AS STRING)
+    ) AS expected_del_meth_src_id,
+
+    rdm.del_meth_src_id,
+    rdm.del_meth_id,
+    rdm.del_meth_src_name,
+    rdm.del_meth_name_conformed
+
 FROM silver_wip_activityentry ae
 
-LEFT JOIN silver_wip_activityheader AH
-    ON ae.activity_header_id = AH.id
+LEFT JOIN silver_wip_activityheader ah
+    ON ae.activity_header_id = ah.id
 
-LEFT JOIN silver_wip_ServiceType SerT
-    ON SerT.id = AH.service_type_id
+LEFT JOIN silver_wip_servicetype st
+    ON ah.service_type_id = st.id
 
-LEFT JOIN silver_wip_activitytype atyp
-    ON ae.activity_type_id = atyp.id
+LEFT JOIN silver_wip_activityservice acs
+    ON ae.activity_service_id = acs.id
 
-LEFT JOIN silver_wip_activityservice actserv
-    ON actserv.id = ae.activity_service_id
+LEFT JOIN silver_wip_service s
+    ON acs.service_id = s.id
 
-LEFT JOIN silver_wip_service srv
-    ON actserv.service_id = srv.id
+LEFT JOIN silver_wip_activitytype at
+    ON ae.activity_type_id = at.id
 
-WHERE SerT.id IS NULL
-   OR srv.id IS NULL
-   OR atyp.id IS NULL
+LEFT JOIN silver_rdm_delivery_method rdm
+    ON LOWER(TRIM(rdm.del_meth_src_id))
+       = LOWER(TRIM(
+           CONCAT(
+               'WIP001_',
+               CAST(st.id AS STRING),
+               '_',
+               CAST(s.id AS STRING),
+               '_',
+               CAST(at.id AS STRING)
+           )
+       ))
 
-GROUP BY
-    SerT.id,
-    srv.id,
-    atyp.id;
+WHERE st.id IS NOT NULL
+  AND s.id IS NOT NULL
+  AND at.id IS NOT NULL
 
-
-
-
-SELECT
-    del_meth_src_id,
-    COUNT(*) AS cnt
-FROM silver_rdm_delivery_method
-WHERE del_meth_src_id LIKE 'WIP001_%'
-GROUP BY del_meth_src_id
-HAVING COUNT(*) > 1
-ORDER BY cnt DESC;
-m
-h
+ORDER BY
+    st.id,
+    s.id,
+    at.id;
