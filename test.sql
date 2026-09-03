04e3d4940d375214db6226b54ebdb30fb5226fdb
Yogita Ganage <yogitaganage19@gmail.com>
2026-09-03 11:03:25 +0100

    g23

--- test.sql
+++ test.sql
@@ -1,34 +1,16 @@
-SELECT
-    CASE
-        WHEN session_del_meth_id IS NULL THEN 'NULL'
-        ELSE 'NOT NULL'
-    END AS session_del_meth_id_status,
-    COUNT(*) AS record_count
-FROM <TEST_TABLE>
-WHERE z_src_system_id = 'MPB'
-GROUP BY
-    CASE
-        WHEN session_del_meth_id IS NULL THEN 'NULL'
-        ELSE 'NOT NULL'
-    END;
-
-
-SELECT 'Production' AS table_name, COUNT(*) AS row_count
-FROM silver_sessions
-WHERE z_src_system_id = 'MPB'
-
-UNION ALL
-
-SELECT 'Test' AS table_name, COUNT(*) AS row_count
-FROM <TEST_TABLE>
-WHERE z_src_system_id = 'MPB';
-
-
-SELECT
-    del_meth_src_id,
-    COUNT(*) AS cnt
-FROM silver_rdm_delivery_method
-WHERE del_meth_src_id LIKE 'MPB001_%'
-GROUP BY del_meth_src_id
-HAVING COUNT(*) > 1
-ORDER BY cnt DESC;
+SELECT DISTINCT
+    appt.appointment_type_id,
+    appttype.name AS appointment_type_name,
+    CONCAT('MPB001_', CAST(appttype.id AS STRING)) AS expected_del_meth_src_id,
+    rdm.del_meth_src_id,
+    rdm.del_meth_id,
+    rdm.del_meth_src_name,
+    rdm.del_meth_name_conformed
+FROM silver_drj_appointments appt
+LEFT JOIN silver_drj_appointment_types appttype
+    ON appttype.id = appt.appointment_type_id
+LEFT JOIN silver_rdm_delivery_method rdm
+    ON LOWER(TRIM(rdm.del_meth_src_id))
+       = LOWER(TRIM(CONCAT('MPB001_', CAST(appttype.id AS STRING))))
+WHERE appttype.id IS NOT NULL
+ORDER BY appt.appointment_type_id;
