847538c2855b260e03d28d0517592d066331256a
Yogita Ganage <yogitaganage19@gmail.com>
2026-09-03 12:51:36 +0100

    g34

--- test.sql
+++ test.sql
@@ -1,29 +1,26 @@
-SELECT DISTINCT
-    ah.file_number AS case_number,
-    ah.id AS activity_header_id,
+SELECT
+    id AS activity_header_id,
+    file_number AS case_number,
+    service_type_id
+FROM silver_wip_activityheader
+WHERE file_number = 549850;
+
+
+SELECT
+    id AS activity_entry_id,
+    activity_header_id,
+    activity_service_id,
+    activity_type_id
+FROM silver_wip_activityentry
+WHERE id = 896860;
+
+SELECT
     ae.id AS activity_entry_id,
-    ah.service_type_id,
-    s.id AS service_id,
-    at.id AS activity_type_id
+    ae.activity_header_id AS entry_activity_header_id,
+    ah.id AS case_activity_header_id,
+    ah.file_number AS case_number,
+    ah.service_type_id
 FROM silver_wip_activityentry ae
-
-LEFT JOIN silver_wip_activityheader ah
-    ON ae.activity_header_id = ah.id
-
-LEFT JOIN silver_wip_servicetype st
-    ON ah.service_type_id = st.id
-
-LEFT JOIN silver_wip_activityservice acs
-    ON ae.activity_service_id = acs.id
-
-LEFT JOIN silver_wip_service s
-    ON acs.service_id = s.id
-
-LEFT JOIN silver_wip_activitytype at
-    ON ae.activity_type_id = at.id
-
-WHERE st.id IS NULL
-  AND s.id IS NOT NULL
-  AND at.id IS NOT NULL
-
-ORDER BY ah.file_number;
+CROSS JOIN silver_wip_activityheader ah
+WHERE ae.id = 896860
+  AND ah.file_number = 549850;
