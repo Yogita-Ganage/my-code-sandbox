aca0255b1be1f124e2e65eece77b18f835e856d3
Yogita Ganage <yogitaganage19@gmail.com>
2026-09-08 18:11:12 +0100

    g58

--- test.sql
+++ test.sql
@@ -1,22 +1,30 @@
 SELECT DISTINCT
-    a.id_organisation_source,
-    a.id_rota,
-    r.id AS rota_table_id,
-    a.rota_type AS appointment_rota_type,
-    r.rota_type AS rota_table_rota_type,
-    s.id AS rota_slot_id,
-    s.rota_slot_type,
-    s.blocked_slot
-FROM silver.silver_sone_srappointment a
-LEFT JOIN silver.silver_sone_srrota r
-    ON a.id_rota = r.id
-LEFT JOIN silver.silver_sone_srrotaslot s
-    ON a.id_rota = s.id_rota
-WHERE CONCAT(
-        'SONE',
-        a.id_organisation_source,
+    id_organisation_source,
+    rota_type,
+    rota_slot_type,
+    cprod_src_id,
+    id_rota_from_rotaslot
+FROM silver.silver_sone_srrotaslot_bridging_to_srappointment
+WHERE cprod_src_id IS NOT NULL;
+
+
+SELECT
+    cprod_src_id,
+    COUNT(DISTINCT CONCAT_WS(
+        '|',
+        TRIM(rota_type),
+        TRIM(rota_slot_type)
+    )) AS name_count,
+    COLLECT_SET(CONCAT_WS(
         '_',
-        CAST(a.id_rota AS STRING),
-        '_',
-        CAST(s.id AS STRING)
-      ) = 'SONET7L2F_682116819_616683726845';
+        TRIM(rota_type),
+        TRIM(rota_slot_type)
+    )) AS cprod_names
+FROM silver.silver_sone_srrotaslot_bridging_to_srappointment
+WHERE cprod_src_id IS NOT NULL
+GROUP BY cprod_src_id
+HAVING COUNT(DISTINCT CONCAT_WS(
+    '|',
+    TRIM(rota_type),
+    TRIM(rota_slot_type)
+)) > 1;
