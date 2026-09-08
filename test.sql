de4f6da2df03f1d374c9d71f6268a64d3965d779
Yogita Ganage <yogitaganage19@gmail.com>
2026-09-08 13:12:17 +0100

    g52

--- test.sql
+++ test.sql
@@ -1,19 +1,10 @@
 SELECT DISTINCT
-    r.id_organisation_source AS care_epi_src_id,
-    rssi_contr.src_sys_ins_org_short_name_conformed AS care_epi_src_name,
-    rdmc.contr_src_id AS rdm_src_id,
-    rdmc.contr_src_name AS rdm_src_name,
-    rdmc.contr_id
+    r.id_organisation_source,
+    rssi_contr.src_sys_inst_src_id,
+    rssi_contr.src_sys_ins_org_short_name_conformed
 FROM silver_sone_srreferralin r
-
 LEFT JOIN silver_rdm_source_system_instance rssi_contr
     ON rssi_contr.src_sys_inst_src_id =
        CONCAT('SONE', CAST(r.id_organisation_source AS STRING))
     AND rssi_contr.src_sys_src_id = 'SONE'
-
-LEFT JOIN silver_rdm_contract rdmc
-    ON TRIM(LOWER(rdmc.contr_src_name))
-       = TRIM(LOWER(rssi_contr.src_sys_ins_org_short_name_conformed))
-
-WHERE rdmc.contr_id IS NOT NULL
 ORDER BY r.id_organisation_source;
