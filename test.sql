64963c37860003a3e0bd5fb993186653d27a9ea6
Yogita Ganage <yogitaganage19@gmail.com>
2026-09-08 18:25:58 +0100

    g62

--- test.sql
+++ test.sql
@@ -1,11 +1,8 @@
 WITH current_name AS (
     SELECT DISTINCT
         a.id_organisation_source,
-        CONCAT(
-            COALESCE(TRIM(a.rota_type), 'Null'),
-            '_',
-            COALESCE(TRIM(s.rota_slot_type), 'Null')
-        ) AS cprod_name
+        TRIM(a.rota_type) AS rota_type,
+        TRIM(s.rota_slot_type) AS rota_slot_type
     FROM silver.silver_sone_srappointment a
     LEFT JOIN silver.silver_sone_srrotaslot s
         ON a.id_rota = s.id_rota
@@ -16,11 +13,8 @@ WITH current_name AS (
 bridge_name AS (
     SELECT DISTINCT
         b.id_organisation_source,
-        CONCAT(
-            COALESCE(TRIM(b.rota_type), 'Null'),
-            '_',
-            COALESCE(TRIM(b.rota_slot_type), 'Null')
-        ) AS cprod_name
+        TRIM(b.rota_type) AS rota_type,
+        TRIM(b.rota_slot_type) AS rota_slot_type
     FROM silver.silver_sone_srrotaslot_bridging_to_srappointment b
 )
 
@@ -28,4 +22,5 @@ SELECT c.*
 FROM current_name c
 LEFT ANTI JOIN bridge_name b
     ON c.id_organisation_source = b.id_organisation_source
-   AND LOWER(TRIM(c.cprod_name)) = LOWER(TRIM(b.cprod_name));
+   AND LOWER(c.rota_type) = LOWER(b.rota_type)
+   AND LOWER(c.rota_slot_type) = LOWER(b.rota_slot_type);
