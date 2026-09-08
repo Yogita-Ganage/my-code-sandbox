0e34e98396b590d811e111f60bf60bcd7ca17492
Yogita Ganage <yogitaganage19@gmail.com>
2026-09-08 18:13:23 +0100

    g59

--- test.sql
+++ test.sql
@@ -1,30 +1,11 @@
-SELECT DISTINCT
-    id_organisation_source,
-    rota_type,
-    rota_slot_type,
-    cprod_src_id,
-    id_rota_from_rotaslot
-FROM silver.silver_sone_srrotaslot_bridging_to_srappointment
-WHERE cprod_src_id IS NOT NULL;
-
-
 SELECT
-    cprod_src_id,
-    COUNT(DISTINCT CONCAT_WS(
-        '|',
-        TRIM(rota_type),
-        TRIM(rota_slot_type)
-    )) AS name_count,
-    COLLECT_SET(CONCAT_WS(
-        '_',
-        TRIM(rota_type),
-        TRIM(rota_slot_type)
-    )) AS cprod_names
+    TRIM(rota_type) AS rota_type,
+    TRIM(rota_slot_type) AS rota_slot_type,
+    COUNT(DISTINCT cprod_src_id) AS id_count,
+    COLLECT_SET(cprod_src_id) AS ids
 FROM silver.silver_sone_srrotaslot_bridging_to_srappointment
 WHERE cprod_src_id IS NOT NULL
-GROUP BY cprod_src_id
-HAVING COUNT(DISTINCT CONCAT_WS(
-    '|',
+GROUP BY
     TRIM(rota_type),
     TRIM(rota_slot_type)
-)) > 1;
+HAVING COUNT(DISTINCT cprod_src_id) > 1;
