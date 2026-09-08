9b5111e3e7aa6e5424675668b15576d57059e3b0
Yogita Ganage <yogitaganage19@gmail.com>
2026-09-08 17:36:49 +0100

    g57

--- test.sql
+++ test.sql
@@ -1,31 +1,22 @@
-SELECT
-    CONCAT(
+SELECT DISTINCT
+    a.id_organisation_source,
+    a.id_rota,
+    r.id AS rota_table_id,
+    a.rota_type AS appointment_rota_type,
+    r.rota_type AS rota_table_rota_type,
+    s.id AS rota_slot_id,
+    s.rota_slot_type,
+    s.blocked_slot
+FROM silver.silver_sone_srappointment a
+LEFT JOIN silver.silver_sone_srrota r
+    ON a.id_rota = r.id
+LEFT JOIN silver.silver_sone_srrotaslot s
+    ON a.id_rota = s.id_rota
+WHERE CONCAT(
         'SONE',
         a.id_organisation_source,
         '_',
         CAST(a.id_rota AS STRING),
         '_',
         CAST(s.id AS STRING)
-    ) AS cprod_src_id,
-
-    s.blocked_slot,
-
-    COUNT(DISTINCT TRIM(a.rota_type)) AS rota_type_count,
-
-    COLLECT_SET(TRIM(a.rota_type)) AS rota_types
-
-FROM silver.silver_sone_srappointment a
-
-LEFT JOIN silver.silver_sone_srrotaslot s
-    ON a.id_rota = s.id_rota
-
-WHERE a.id IS NOT NULL
-  AND a.id_organisation_source IS NOT NULL
-
-GROUP BY
-    a.id_organisation_source,
-    a.id_rota,
-    s.id,
-    s.blocked_slot
-
-HAVING COUNT(DISTINCT TRIM(a.rota_type)) > 1;
+      ) = 'SONET7L2F_682116819_616683726845';
