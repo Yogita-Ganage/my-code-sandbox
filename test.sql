a29b18224c0e2af39a5a7cdb85c38d8cadb6a6e6
Yogita Ganage <yogitaganage19@gmail.com>
2026-09-08 13:20:41 +0100

    g53

--- test.sql
+++ test.sql
@@ -1,10 +1,30 @@
-SELECT DISTINCT
-    r.id_organisation_source,
-    rssi_contr.src_sys_inst_src_id,
-    rssi_contr.src_sys_ins_org_short_name_conformed
-FROM silver_sone_srreferralin r
-LEFT JOIN silver_rdm_source_system_instance rssi_contr
-    ON rssi_contr.src_sys_inst_src_id =
-       CONCAT('SONE', CAST(r.id_organisation_source AS STRING))
-    AND rssi_contr.src_sys_src_id = 'SONE'
-ORDER BY r.id_organisation_source;
+SELECT
+    contr_src_name,
+    contr_src_id,
+    contr_id
+FROM silver_rdm_contract
+WHERE UPPER(TRIM(contr_src_name)) IN (
+    'DERM COVENTRY',
+    'PENNINE MSK',
+    'MSK SEL',
+    'DERM SEFTON',
+    'B2C DERM',
+    'DERM CALDERDALE'
+)
+ORDER BY contr_src_name;
+
+
+SELECT
+    contr_src_name,
+    contr_src_id,
+    contr_src_sys_inst_id
+FROM silver_rdm_contract_add
+WHERE UPPER(TRIM(contr_src_name)) IN (
+    'DERM COVENTRY',
+    'PENNINE MSK',
+    'MSK SEL',
+    'DERM SEFTON',
+    'B2C DERM',
+    'DERM CALDERDALE'
+)
+ORDER BY contr_src_name;
