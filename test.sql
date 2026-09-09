1e447a816fbbdb06054c829463ecb1c6377e1142
Yogita Ganage <yogitaganage19@gmail.com>
2026-09-09 14:31:53 +0100

    g67

--- test.sql
+++ test.sql
@@ -1,18 +1,16 @@
-SELECT
+SELECT DISTINCT
+    a.id AS appointment_id,
+    a.id_referral_in AS case_reference,
     a.id_organisation_source,
-    COUNT(DISTINCT a.id) AS record_count
+    a.date_start,
+    a.rota_type,
+    s.rota_slot_type
 FROM silver.silver_sone_srappointment a
 
 LEFT JOIN silver.silver_sone_srrotaslot s
     ON a.id_rota = s.id_rota
 
-WHERE a.id IS NOT NULL
-  AND a.id_organisation_source IS NOT NULL
-  AND COALESCE(TRIM(a.rota_type), '') = ''
+WHERE COALESCE(TRIM(a.rota_type), '') = ''
   AND COALESCE(TRIM(s.rota_slot_type), '') = ''
 
-GROUP BY
-    a.id_organisation_source
-
-ORDER BY
-    a.id_organisation_source;
+LIMIT 10;
