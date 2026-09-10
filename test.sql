dcbcb0cc1c783db6f5dea626a247ab11e893c79d
Yogita Ganage <yogitaganage19@gmail.com>
2026-09-10 16:29:25 +0100

    g77

--- test.sql
+++ test.sql
@@ -1,5 +1,15 @@
-SELECT
-    id,
-    business_actor_id
-FROM silver_wip_businessrole
-WHERE id = 167197;
+AHRD.customer_id = 167197
+        ↓
+silver_wip_businessrole.id = 167197
+        ↓
+business_actor_id = 163010
+        ↓
+silver_wip_organisation.id = 163010
+
+
+
+I checked the failed WIP session UAT issue. The examples from UAT were not showing now, but when I checked all WIP sessions, I found 82 duplicate source session IDs.
+
+I traced one example and found the duplication is coming from the organisation/contract join using only the name. We’ve now found the correct relationship using the customer ID and business role.
+
+Next I’ll update and validate the join, check that the duplicates are removed, and then confirm the approach with Sean. If all looks good, I should be able to finish the task today.
