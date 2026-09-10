774d11c3edfa61baa0a1ef58824afd2826b2137b
Yogita Ganage <yogitaganage19@gmail.com>
2026-09-10 15:56:45 +0100

    g74

--- test.sql
+++ test.sql
@@ -1,14 +1,11 @@
-DESCRIBE silver_wip_activityheaderroledetail;
+SELECT *
+FROM silver_wip_organisation
+WHERE id IN (163008, 163010);
 
 
+DESCRIBE silver_wip_organisation;
 
-SELECT
-    ae.id AS activity_entry_id,
-    ah.id AS activity_header_id,
-    AHRD.*
-FROM silver_wip_activityentry ae
-LEFT JOIN silver_wip_activityheader ah
-    ON ae.activity_header_id = ah.id
-LEFT JOIN silver_wip_activityheaderroledetail AHRD
-    ON ah.id = AHRD.activity_header_id
-WHERE ae.id = 505044;
+
+SELECT *
+FROM silver_wip_organisation
+WHERE id = 167197;
