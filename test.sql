c998d2ed330469933ef10ee05e71a0c1197c85b3
Yogita Ganage <yogitaganage19@gmail.com>
2026-09-10 14:39:45 +0100

    g71

--- test.sql
+++ test.sql
@@ -3,10 +3,6 @@ SELECT
     COUNT(*) AS row_count
 FROM silver_sessions
 WHERE z_src_system_id = 'WIP'
-  AND src_session_id IN ('WIP986532', 'WIP962593')
-GROUP BY src_session_id;
-
-
-SELECT *
-FROM silver_sessions
-WHERE src_session_id IN ('WIP986532', 'WIP962593');
+GROUP BY src_session_id
+HAVING COUNT(*) > 1
+ORDER BY row_count DESC;
