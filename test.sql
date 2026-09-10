13561af1fed012855fd60f0c2d1f563b36c424b5
Yogita Ganage <yogitaganage19@gmail.com>
2026-09-10 16:17:01 +0100

    g76

--- test.sql
+++ test.sql
@@ -1,26 +1,5 @@
-
-name-only organisation join one-to-many
-
-WIP505044
-   ↓
-AHRD 1 row
-customer_id   = 167197
-customer_name = Ecclesiastical
-   ↓
-silver_wip_organisation
-name = Ecclesiastical
-   ↓
-2 rows:
-163008
-163010
-
-
-
-
 SELECT
-    contr_id,
-    contr_name,
-    contr_src_id,
-    contr_src_name
-FROM silver_contract
-WHERE LOWER(TRIM(contr_name)) = 'wip ecclesiastical';
+    id,
+    business_actor_id
+FROM silver_wip_businessrole
+WHERE id = 167197;
