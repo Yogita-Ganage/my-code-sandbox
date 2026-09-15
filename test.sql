c68729205960fb5c61b002bb610727f5b435ab8f
Yogita Ganage <yogitaganage19@gmail.com>
2026-09-15 15:42:34 +0100

    g16

--- test.sql
+++ test.sql
@@ -1,9 +1,12 @@
+%%sql
+
 SELECT
-    src_session_id,
-    COUNT(*) AS row_count,
-    COUNT(DISTINCT session_patient_id) AS patient_count
-FROM silver_sessiony_test
-WHERE z_src_system_id = 'WIP'
-GROUP BY src_session_id
+    cprod_src_id,
+    COUNT(*) AS cnt,
+    COUNT(DISTINCT cprod_src_sys_inst_src_id) AS instance_cnt,
+    COUNT(DISTINCT cprod_src_name) AS name_cnt
+FROM silver_rdm_care_product
+WHERE cprod_src_sys_inst_src_id LIKE 'SONE%'
+GROUP BY cprod_src_id
 HAVING COUNT(*) > 1
-ORDER BY patient_count DESC, src_session_id;
+ORDER BY cnt DESC;
