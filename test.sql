490fd2d5d0740a643589e5e603dac8cd17cf59ca
Yogita Ganage <yogitaganage19@gmail.com>
2026-09-15 15:44:31 +0100

    g17

--- test.sql
+++ test.sql
@@ -1,12 +1,13 @@
 %%sql
 
 SELECT
-    cprod_src_id,
-    COUNT(*) AS cnt,
-    COUNT(DISTINCT cprod_src_sys_inst_src_id) AS instance_cnt,
-    COUNT(DISTINCT cprod_src_name) AS name_cnt
-FROM silver_rdm_care_product
-WHERE cprod_src_sys_inst_src_id LIKE 'SONE%'
-GROUP BY cprod_src_id
-HAVING COUNT(*) > 1
-ORDER BY cnt DESC;
+    src_session_id,
+    COUNT(*) AS row_cnt,
+    COUNT(DISTINCT cprod_src_id) AS cprod_cnt,
+    COLLECT_SET(cprod_src_id) AS cprod_src_ids
+FROM silver_sone_srrotaslot_bridging_to_srappointment
+WHERE src_session_id IN (
+    'SONE00D1Z35100345160',
+    'SONE00D1Z35137356890'
+)
+GROUP BY src_session_id;
