377a2783e40d2be41382746405fd2f1f6e091460
Yogita Ganage <yogitaganage19@gmail.com>
2026-09-15 16:28:38 +0100

    g26

--- test.sql
+++ test.sql
@@ -1,14 +1,24 @@
 %%sql
 
-SELECT *
-FROM silver_rdm_care_product
-WHERE cprod_id IN (
-    4705, 7952,
-    4726, 7994,
-    4966, 8279,
-    5116, 8936,
-    5135, 8976,
-    5158, 9019,
-    5247, 9260
+WITH duplicate_groups AS (
+
+    SELECT
+        LOWER(TRIM(cprod_src_sys_inst_src_id)) AS src_instance,
+        LOWER(TRIM(cprod_src_id)) AS normalized_cprod_src_id,
+        COUNT(*) AS cnt
+
+    FROM silver_rdm_care_product
+
+    WHERE LOWER(TRIM(cprod_src_sys_inst_src_id)) LIKE 'sone%'
+
+    GROUP BY
+        LOWER(TRIM(cprod_src_sys_inst_src_id)),
+        LOWER(TRIM(cprod_src_id))
+
+    HAVING COUNT(*) > 1
 )
-ORDER BY LOWER(TRIM(cprod_src_id)), cprod_id;
+
+SELECT
+    COUNT(*) AS duplicate_groups,
+    SUM(cnt - 1) AS extra_rows_to_review
+FROM duplicate_groups;
