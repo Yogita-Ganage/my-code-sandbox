979596f93eddeac782007a73ec760075c104b961
Yogita Ganage <yogitaganage19@gmail.com>
2026-09-15 16:21:34 +0100

    g25

--- test.sql
+++ test.sql
@@ -1,15 +1,14 @@
 %%sql
 
-SELECT
-    LOWER(TRIM(cprod_src_sys_inst_src_id)) AS src_instance,
-    LOWER(TRIM(cprod_src_id)) AS normalized_cprod_src_id,
-    COUNT(*) AS row_cnt,
-    COLLECT_SET(cprod_id) AS cprod_ids,
-    COLLECT_SET(cprod_src_id) AS original_values
+SELECT *
 FROM silver_rdm_care_product
-WHERE cprod_src_sys_inst_src_id LIKE 'SONE%'
-GROUP BY
-    LOWER(TRIM(cprod_src_sys_inst_src_id)),
-    LOWER(TRIM(cprod_src_id))
-HAVING COUNT(*) > 1
-ORDER BY row_cnt DESC;
+WHERE cprod_id IN (
+    4705, 7952,
+    4726, 7994,
+    4966, 8279,
+    5116, 8936,
+    5135, 8976,
+    5158, 9019,
+    5247, 9260
+)
+ORDER BY LOWER(TRIM(cprod_src_id)), cprod_id;
