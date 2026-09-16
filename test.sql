f3e0ffe892e0df527a472ae0305ea05a7f952acb
Yogita Ganage <yogitaganage19@gmail.com>
2026-09-16 15:01:31 +0100

    g37

--- test.sql
+++ test.sql
@@ -18,3 +18,32 @@ SELECT
 FROM silver_sone_srappointment
 GROUP BY COALESCE(removed_data, 2)
 ORDER BY removed_data_group;
+
+
+
+%%sql
+
+WITH grp AS (
+    SELECT
+        id_organisation_source,
+        id,
+        MAX(CASE WHEN removed_data = 0 THEN 1 ELSE 0 END) AS has_0,
+        MAX(CASE WHEN removed_data = 1 THEN 1 ELSE 0 END) AS has_1,
+        MAX(CASE WHEN removed_data IS NULL THEN 1 ELSE 0 END) AS has_null
+    FROM silver_sone_srappointment
+    GROUP BY id_organisation_source, id
+),
+final AS (
+    SELECT *,
+        CASE
+            WHEN has_0 = 1 AND has_1 = 1 THEN 'Both 0 and 1'
+            WHEN has_0 = 1 AND has_1 = 0 AND has_null = 0 THEN 'Only 0'
+            WHEN has_0 = 0 AND has_1 = 1 AND has_null = 0 THEN 'Only 1'
+            WHEN has_0 = 0 AND has_1 = 0 AND has_null = 1 THEN 'Only NULL'
+            ELSE 'Mixed with NULL'
+        END AS group_type
+    FROM grp
+)
+SELECT group_type, COUNT(*) AS session_count
+FROM final
+GROUP BY group_type;
