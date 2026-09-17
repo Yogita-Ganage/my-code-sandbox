f5476a4e77599e8759d9a36553f95efd60a99e49
Yogita Ganage <yogitaganage19@gmail.com>
2026-09-17 14:18:49 +0100

    g40

--- test.sql
+++ test.sql
@@ -1,21 +1,13 @@
-%%sql
+-- Keep the latest active primary referral reason per referral to prevent duplicate session rows
 
-SELECT
-    a.id,
-    a.id_organisation_source,
-    a.id_referral_in,
-    rr.referral_reason,
-    cfg.configured_list_option,
-    rr.primary_referral_reason,
-    rr.date_removed,
-    rr.date_event_recorded
-FROM silver_sone_srappointment a
-LEFT JOIN silver_sone_srreferralinreferralreason rr
-    ON rr.id_referral_in = a.id_referral_in
-   AND rr.primary_referral_reason = 1
-   AND rr.date_removed IS NULL
-LEFT JOIN silver_sone_srconfiguredlistoption cfg
-    ON cfg.id = rr.referral_reason
-WHERE a.id IN (35161102865, 35156579502)
-  AND a.id_organisation_source = 'NLF11'
-ORDER BY a.id, rr.date_event_recorded;
+LEFT JOIN (
+    SELECT * FROM (
+        SELECT *, ROW_NUMBER() OVER (
+            PARTITION BY id_referral_in
+            ORDER BY date_event_recorded DESC, id DESC
+        ) rn
+        FROM silver_sone_srreferralinreferralreason
+        WHERE primary_referral_reason = 1 AND date_removed IS NULL
+    ) x WHERE rn = 1
+) srrefcr
+ON srrefcr.id_referral_in = sra.id_referral_in
