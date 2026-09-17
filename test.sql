ac51d9c8b523cd87577e4e8060b0e981d51ce54b
Yogita Ganage <yogitaganage19@gmail.com>
2026-09-17 13:19:46 +0100

    g39

--- test.sql
+++ test.sql
@@ -1,30 +1,21 @@
 %%sql
 
 SELECT
-    id,
-    id_organisation_source,
-    id_referral_in
-FROM silver_sone_srappointment
-WHERE id = 35161102865
-  AND id_organisation_source = 'NLF11';
-
-  %%sql
-
-SELECT *
-FROM silver_sone_srreferralinreferralreason
-WHERE id_referral_in = < id_referral_in>
-  AND primary_referral_reason = 1
-  AND date_removed IS NULL;
-
-
-  %%sql
-
-SELECT
+    a.id,
+    a.id_organisation_source,
+    a.id_referral_in,
     rr.referral_reason,
-    cfg.configured_list_option
-FROM silver_sone_srreferralinreferralreason rr
+    cfg.configured_list_option,
+    rr.primary_referral_reason,
+    rr.date_removed,
+    rr.date_event_recorded
+FROM silver_sone_srappointment a
+LEFT JOIN silver_sone_srreferralinreferralreason rr
+    ON rr.id_referral_in = a.id_referral_in
+   AND rr.primary_referral_reason = 1
+   AND rr.date_removed IS NULL
 LEFT JOIN silver_sone_srconfiguredlistoption cfg
     ON cfg.id = rr.referral_reason
-WHERE rr.id_referral_in = < id_referral_in>
-  AND rr.primary_referral_reason = 1
-  AND rr.date_removed IS NULL;
+WHERE a.id IN (35161102865, 35156579502)
+  AND a.id_organisation_source = 'NLF11'
+ORDER BY a.id, rr.date_event_recorded;
