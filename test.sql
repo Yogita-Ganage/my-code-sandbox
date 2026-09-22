bbc245320977103f1aa2f498e78066c833d6f108
Yogita Ganage <yogitaganage19@gmail.com>
2026-09-22 18:35:58 +0100

    g58

--- test.sql
+++ test.sql
@@ -1,13 +1,9 @@
 SELECT
-    id AS appointment_id,
-    rota_type,
-    follow_up_appointment,
-    telephone_appointment,
-    CONCAT_WS(
-        '_',
-        NULLIF(TRIM(rota_type), ''),
-        CASE WHEN follow_up_appointment = true THEN 'Follow-up Appointment' END,
-        CASE WHEN telephone_appointment = true THEN 'Telephone Appointment' END
-    ) AS actual_del_meth_src_name
-FROM silver_sone_srappointment
-WHERE id = 15889670415;
+    id,
+    id_mapping_group,
+    mapping,
+    id_organisation_source
+FROM silver_sone_srmapping
+WHERE LOWER(mapping) LIKE '%walk%'
+   OR LOWER(mapping) LIKE '%follow%'
+   OR LOWER(mapping) LIKE '%telephone%';
