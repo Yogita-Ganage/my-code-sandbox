8f356fdcc8f14db82d209e682a3ad1396b3e77d7
Yogita Ganage <yogitaganage19@gmail.com>
2026-09-23 12:38:21 +0100

    g59

--- test.sql
+++ test.sql
@@ -1,9 +1,6 @@
-SELECT
-    id,
-    id_mapping_group,
-    mapping,
-    id_organisation_source
-FROM silver_sone_srmapping
-WHERE LOWER(mapping) LIKE '%walk%'
-   OR LOWER(mapping) LIKE '%follow%'
-   OR LOWER(mapping) LIKE '%telephone%';
+from notebookutils import mssparkutils
+
+mssparkutils.fs.cp(
+    "Files/bronze_Control_file_lakshmi.csv",
+    "Files/bronze_Control_file_veri.csv"
+)
