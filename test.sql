5ebd2a894c7d4a42dbe59f2ec89c28ea2747bf8c
Yogita Ganage <yogitaganage19@gmail.com>
2026-09-24 13:37:03 +0100

    g63

--- test.sql
+++ test.sql
@@ -6,3 +6,17 @@ FROM bronze_sone_srappointmentflags
 GROUP BY RowIdentifier
 HAVING COUNT(DISTINCT id_organisation_source) > 1
 ORDER BY organisation_count DESC;
+
+
+
+SELECT
+    RowIdentifier,
+    id_organisation_source,
+    FILEDATE,
+    to_date(
+        reverse(substring(reverse(FILEDATE), 5, 8)),
+        'yyyyMMdd'
+    ) AS file_date
+FROM bronze_sone_srappointmentflags
+WHERE RowIdentifier = 2284411412712
+ORDER BY id_organisation_source, file_date DESC;
